f928bc6dd7a2eb470cd61f8998c98d2b7da68eef
samuelleblanc <samuel.elie.leblanc@gmail.com>
2015-02-03 17:51:28 -0800

    start of run_libradtran for 2.0

---
+++ Run_libradtran.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <markdowncell>
+
+# Name:  
+# 
+#     Run_libradtran
+# 
+# Purpose:  
+# 
+#     Python modules that combine the different modules used for writing and reading libradtran files. 
+#     Used with libradtran 2.0
+#     Same outputs as IDL procedures 'write_inp_mix.pro' but for libradtran 2.0
+# 
+# Calling Sequence:
+# 
+#     import Run_libradtran as RL
+#   
+# Input:
+# 
+#     none at command line
+#     see methods of module
+# 
+# Output:
+#    
+#     input files for libradtran 2.0 (uvspec) 
+#   
+# Keywords:
+# 
+#     none
+#   
+# Dependencies:
+# 
+#     - numpy
+#     - scipy : for saving and reading
+#     - math
+#     - pdb
+#     - datetime
+# 
+#   
+# Needed Files:
+# 
+#   - ...
+
+# <codecell>
+
+
