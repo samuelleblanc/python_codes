a9f06d97ddf1ad6d834409915a8169f0424fb25b
samuelleblanc <samuel.elie.leblanc@gmail.com>
2015-07-28 18:45:24 -0700

    playing with interactive paths

--- Interactive_map.py
+++ Interactive_map.py
@@ -212,4 +212,424 @@ wb = Create_excel()
 
 # <codecell>
 
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+class DraggablePoint:
+    lock = None #only one can be animated at a time
+    def __init__(self, point):
+        self.point = point
+        self.press = None
+        self.background = None
+
+    def connect(self):
+        'connect to all the events we need'
+        self.cidpress = self.point.figure.canvas.mpl_connect('button_press_event', self.on_press)
+        self.cidrelease = self.point.figure.canvas.mpl_connect('button_release_event', self.on_release)
+        self.cidmotion = self.point.figure.canvas.mpl_connect('motion_notify_event', self.on_motion)
+
+    def on_press(self, event):
+        if event.inaxes != self.point.axes: return
+        if DraggablePoint.lock is not None: return
+        contains, attrd = self.point.contains(event)
+        if not contains: return
+        self.press = (self.point.center), event.xdata, event.ydata
+        DraggablePoint.lock = self
+
+        # draw everything but the selected rectangle and store the pixel buffer
+        canvas = self.point.figure.canvas
+        axes = self.point.axes
+        self.point.set_animated(True)
+        canvas.draw()
+        self.background = canvas.copy_from_bbox(self.point.axes.bbox)
+
+        # now redraw just the rectangle
+        axes.draw_artist(self.point)
+
+        # and blit just the redrawn area
+        canvas.blit(axes.bbox)
+
+    def on_motion(self, event):
+        if DraggablePoint.lock is not self:
+            return
+        if event.inaxes != self.point.axes: return
+        self.point.center, xpress, ypress = self.press
+        dx = event.xdata - xpress
+        dy = event.ydata - ypress
+        self.point.center = (self.point.center[0]+dx, self.point.center[1]+dy)
+
+        canvas = self.point.figure.canvas
+        axes = self.point.axes
+        # restore the background region
+        canvas.restore_region(self.background)
+
+        # redraw just the current rectangle
+        axes.draw_artist(self.point)
+
+        # blit just the redrawn area
+        canvas.blit(axes.bbox)
+
+    def on_release(self, event):
+        'on release we reset the press data'
+        if DraggablePoint.lock is not self:
+            return
+
+        self.press = None
+        DraggablePoint.lock = None
+
+        # turn off the rect animation property and reset the background
+        self.point.set_animated(False)
+        self.background = None
+
+        # redraw the full figure
+        self.point.figure.canvas.draw()
+
+    def disconnect(self):
+        'disconnect all the stored connection ids'
+        self.point.figure.canvas.mpl_disconnect(self.cidpress)
+        self.point.figure.canvas.mpl_disconnect(self.cidrelease)
+        self.point.figure.canvas.mpl_disconnect(self.cidmotion)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+drs = []
+circles = [patches.Circle((0.32, 0.3), 0.03, fc='r', alpha=0.5),
+               patches.Circle((0.3,0.3), 0.03, fc='g', alpha=0.5)]
+
+for circ in circles:
+    ax.add_patch(circ)
+    dr = DraggablePoint(circ)
+    dr.connect()
+    drs.append(dr)
+
+plt.show()
+
+# <codecell>
+
+"""
+This is an example to show how to build cross-GUI applications using
+matplotlib event handling to interact with objects on the canvas
+
+"""
+import numpy as np
+from matplotlib.lines import Line2D
+from matplotlib.artist import Artist
+from matplotlib.mlab import dist_point_to_segment
+
+
+class PolygonInteractor:
+    """
+    An polygon editor.
+
+    Key-bindings
+
+      't' toggle vertex markers on and off.  When vertex markers are on,
+          you can move them, delete them
+
+      'd' delete the vertex under point
+
+      'i' insert a vertex at point.  You must be within epsilon of the
+          line connecting two existing vertices
+
+    """
+
+    showverts = True
+    epsilon = 5  # max pixel distance to count as a vertex hit
+
+    def __init__(self, ax, poly):
+        if poly.figure is None:
+            raise RuntimeError('You must first add the polygon to a figure or canvas before defining the interactor')
+        self.ax = ax
+        canvas = poly.figure.canvas
+        self.poly = poly
+
+        x, y = zip(*self.poly.xy)
+        self.line = Line2D(x, y, marker='o', markerfacecolor='r', animated=True)
+        self.ax.add_line(self.line)
+        #self._update_line(poly)
+
+        cid = self.poly.add_callback(self.poly_changed)
+        self._ind = None # the active vert
+
+        canvas.mpl_connect('draw_event', self.draw_callback)
+        canvas.mpl_connect('button_press_event', self.button_press_callback)
+        canvas.mpl_connect('key_press_event', self.key_press_callback)
+        canvas.mpl_connect('button_release_event', self.button_release_callback)
+        canvas.mpl_connect('motion_notify_event', self.motion_notify_callback)
+        self.canvas = canvas
+
+
+    def draw_callback(self, event):
+        self.background = self.canvas.copy_from_bbox(self.ax.bbox)
+        self.ax.draw_artist(self.poly)
+        self.ax.draw_artist(self.line)
+        self.canvas.blit(self.ax.bbox)
+
+    def poly_changed(self, poly):
+        'this method is called whenever the polygon object is called'
+        # only copy the artist props to the line (except visibility)
+        vis = self.line.get_visible()
+        Artist.update_from(self.line, poly)
+        self.line.set_visible(vis)  # don't use the poly visibility state
+
+
+    def get_ind_under_point(self, event):
+        'get the index of the vertex under point if within epsilon tolerance'
+
+        # display coords
+        xy = np.asarray(self.poly.xy)
+        xyt = self.poly.get_transform().transform(xy)
+        xt, yt = xyt[:, 0], xyt[:, 1]
+        d = np.sqrt((xt-event.x)**2 + (yt-event.y)**2)
+        indseq = np.nonzero(np.equal(d, np.amin(d)))[0]
+        ind = indseq[0]
+
+        if d[ind]>=self.epsilon:
+            ind = None
+
+        return ind
+
+    def button_press_callback(self, event):
+        'whenever a mouse button is pressed'
+        if not self.showverts: return
+        if event.inaxes==None: return
+        if event.button != 1: return
+        self._ind = self.get_ind_under_point(event)
+
+    def button_release_callback(self, event):
+        'whenever a mouse button is released'
+        if not self.showverts: return
+        if event.button != 1: return
+        self._ind = None
+
+    def key_press_callback(self, event):
+        'whenever a key is pressed'
+        if not event.inaxes: return
+        if event.key=='t':
+            self.showverts = not self.showverts
+            self.line.set_visible(self.showverts)
+            if not self.showverts: self._ind = None
+        elif event.key=='d':
+            ind = self.get_ind_under_point(event)
+            if ind is not None:
+                self.poly.xy = [tup for i,tup in enumerate(self.poly.xy) if i!=ind]
+                self.line.set_data(zip(*self.poly.xy))
+        elif event.key=='i':
+            xys = self.poly.get_transform().transform(self.poly.xy)
+            p = event.x, event.y # display coords
+            for i in range(len(xys)-1):
+                s0 = xys[i]
+                s1 = xys[i+1]
+                d = dist_point_to_segment(p, s0, s1)
+                if d<=self.epsilon:
+                    self.poly.xy = np.array(
+                        list(self.poly.xy[:i]) +
+                        [(event.xdata, event.ydata)] +
+                        list(self.poly.xy[i:]))
+                    self.line.set_data(zip(*self.poly.xy))
+                    break
+
+
+        self.canvas.draw()
+
+    def motion_notify_callback(self, event):
+        'on mouse movement'
+        if not self.showverts: return
+        if self._ind is None: return
+        if event.inaxes is None: return
+        if event.button != 1: return
+        x,y = event.xdata, event.ydata
+
+        self.poly.xy[self._ind] = x,y
+        self.line.set_data(zip(*self.poly.xy))
+
+        self.canvas.restore_region(self.background)
+        self.ax.draw_artist(self.poly)
+        self.ax.draw_artist(self.line)
+        self.canvas.blit(self.ax.bbox)
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+    from matplotlib.patches import Polygon
+
+    theta = np.arange(0, 2*np.pi, 0.1)
+    r = 1.5
+
+    xs = r*np.cos(theta)
+    ys = r*np.sin(theta)
+
+    poly = Polygon(list(zip(xs, ys)), animated=True)
+
+    fig, ax = plt.subplots()
+    ax.add_patch(poly)
+    p = PolygonInteractor(ax, poly)
+
+    #ax.add_line(p.line)
+    ax.set_title('Click and drag a point to move it')
+    ax.set_xlim((-2,2))
+    ax.set_ylim((-2,2))
+    plt.show()
+
+# <codecell>
+
+import numpy as np
+import matplotlib.path as mpath
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+
+Path = mpath.Path
+
+fig, ax = plt.subplots()
+
+pathdata = [
+    (Path.MOVETO, (1.58, -2.57)),
+    (Path.CURVE4, (0.35, -1.1)),
+    (Path.CURVE4, (-1.75, 2.0)),
+    (Path.CURVE4, (0.375, 2.0)),
+    (Path.LINETO, (0.85, 1.15)),
+    (Path.CURVE4, (2.2, 3.2)),
+    (Path.CURVE4, (3, 0.05)),
+    (Path.CURVE4, (2.0, -0.5)),
+    (Path.CLOSEPOLY, (1.58, -2.57)),
+    ]
+
+codes, verts = zip(*pathdata)
+path = mpath.Path(verts, codes)
+patch = mpatches.PathPatch(path, facecolor='green', edgecolor='yellow', alpha=0.5)
+ax.add_patch(patch)
+
+
+class PathInteractor:
+    """
+    An path editor.
+
+    Key-bindings
+
+      't' toggle vertex markers on and off.  When vertex markers are on,
+          you can move them, delete them
+
+
+    """
+
+    showverts = True
+    epsilon = 5  # max pixel distance to count as a vertex hit
+
+    def __init__(self, pathpatch):
+
+        self.ax = pathpatch.axes
+        canvas = self.ax.figure.canvas
+        self.pathpatch = pathpatch
+        self.pathpatch.set_animated(True)
+
+        x, y = zip(*self.pathpatch.get_path().vertices)
+
+        self.line, = ax.plot(x,y,marker='o', markerfacecolor='r', animated=True)
+
+        self._ind = None # the active vert
+
+        canvas.mpl_connect('draw_event', self.draw_callback)
+        canvas.mpl_connect('button_press_event', self.button_press_callback)
+        canvas.mpl_connect('key_press_event', self.key_press_callback)
+        canvas.mpl_connect('button_release_event', self.button_release_callback)
+        canvas.mpl_connect('motion_notify_event', self.motion_notify_callback)
+        self.canvas = canvas
+
+
+    def draw_callback(self, event):
+        self.background = self.canvas.copy_from_bbox(self.ax.bbox)
+        self.ax.draw_artist(self.pathpatch)
+        self.ax.draw_artist(self.line)
+        self.canvas.blit(self.ax.bbox)
+
+    def pathpatch_changed(self, pathpatch):
+        'this method is called whenever the pathpatchgon object is called'
+        # only copy the artist props to the line (except visibility)
+        vis = self.line.get_visible()
+        plt.Artist.update_from(self.line, pathpatch)
+        self.line.set_visible(vis)  # don't use the pathpatch visibility state
+
+
+    def get_ind_under_point(self, event):
+        'get the index of the vertex under point if within epsilon tolerance'
+
+        # display coords
+        xy = np.asarray(self.pathpatch.get_path().vertices)
+        xyt = self.pathpatch.get_transform().transform(xy)
+        xt, yt = xyt[:, 0], xyt[:, 1]
+        d = np.sqrt((xt-event.x)**2 + (yt-event.y)**2)
+        ind = d.argmin()
+
+        if d[ind]>=self.epsilon:
+            ind = None
+
+        return ind
+
+    def button_press_callback(self, event):
+        'whenever a mouse button is pressed'
+        if not self.showverts: return
+        if event.inaxes==None: return
+        if event.button != 1: return
+        self._ind = self.get_ind_under_point(event)
+
+    def button_release_callback(self, event):
+        'whenever a mouse button is released'
+        if not self.showverts: return
+        if event.button != 1: return
+        self._ind = None
+
+    def key_press_callback(self, event):
+        'whenever a key is pressed'
+        if not event.inaxes: return
+        if event.key=='t':
+            self.showverts = not self.showverts
+            self.line.set_visible(self.showverts)
+            if not self.showverts: self._ind = None
+
+        self.canvas.draw()
+
+    def motion_notify_callback(self, event):
+        'on mouse movement'
+        if not self.showverts: return
+        if self._ind is None: return
+        if event.inaxes is None: return
+        if event.button != 1: return
+        x,y = event.xdata, event.ydata
+
+        vertices = self.pathpatch.get_path().vertices
+
+        vertices[self._ind] = x,y
+        self.line.set_data(zip(*vertices))
+
+        self.canvas.restore_region(self.background)
+        self.ax.draw_artist(self.pathpatch)
+        self.ax.draw_artist(self.line)
+        self.canvas.blit(self.ax.bbox)
+
+
+interactor = PathInteractor(patch)
+ax.set_title('drag vertices to update path')
+ax.set_xlim(-3,4)
+ax.set_ylim(-3,4)
+
+plt.show()
+
+# <codecell>
+
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
+
+
+def main():
+    url = 'http://map1c.vis.earthdata.nasa.gov/wmts-geo/wmts.cgi'
+    layer = 'VIIRS_CityLights_2012'
+
+    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax.add_wmts(url, layer)
+    ax.set_extent((-15, 25, 35, 60))
+
+    plt.title('Suomi NPP Earth at night April/October 2012')
+    plt.show()
+
+# <codecell>
+
 
