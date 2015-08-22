02ff1ccee8df22eb910e94964bee231b31a54a37
samuelleblanc <samuel.elie.leblanc@gmail.com>
2015-08-21 18:19:15 -0700

    updated basemap plotting for circles on maps

--- map_utils.py
+++ map_utils.py
@@ -176,3 +176,128 @@ def stats_within_radius(lat1,lon1,lat2,lon2,x2,radius,subset=True):
     print out.keys()
     return out
 
+# <codecell>
+
+def equi(self, m, centerlon, centerlat, radius, *args, **kwargs):
+    """
+    plot a single circle on a map
+    uses the shoot function below
+    from: http://www.geophysique.be/2011/02/20/matplotlib-basemap-tutorial-09-drawing-circles/
+    by: Thomas Lecocq
+    """
+    from map_utils import shoot
+    glon1 = centerlon
+    glat1 = centerlat
+    X = []
+    Y = []
+    for azimuth in range(0, 360):
+        glon2, glat2, baz = shoot(glon1, glat1, azimuth, radius)
+        X.append(glon2)
+        Y.append(glat2)
+    X.append(X[0])
+    Y.append(Y[0])
+
+    #m.plot(X,Y,**kwargs) #Should work, but doesn't...
+    X,Y = m(X,Y)
+    m.plot(X,Y,**kwargs)
+
+# <codecell>
+
+def shoot(lon, lat, azimuth, maxdist=None):
+    """Shooter Function
+    Original javascript on http://williams.best.vwh.net/gccalc.htm
+    Translated to python by Thomas Lecocq
+    """
+    import numpy as np
+    glat1 = lat * np.pi / 180.
+    glon1 = lon * np.pi / 180.
+    s = maxdist / 1.852
+    faz = azimuth * np.pi / 180.
+ 
+    EPS= 0.00000000005
+    if ((np.abs(np.cos(glat1))<EPS) and not (np.abs(np.sin(faz))<EPS)):
+        alert("Only N-S courses are meaningful, starting at a pole!")
+ 
+    a=6378.13/1.852
+    f=1/298.257223563
+    r = 1 - f
+    tu = r * np.tan(glat1)
+    sf = np.sin(faz)
+    cf = np.cos(faz)
+    if (cf==0):
+        b=0.
+    else:
+        b=2. * np.arctan2 (tu, cf)
+ 
+    cu = 1. / np.sqrt(1 + tu * tu)
+    su = tu * cu
+    sa = cu * sf
+    c2a = 1 - sa * sa
+    x = 1. + np.sqrt(1. + c2a * (1. / (r * r) - 1.))
+    x = (x - 2.) / x
+    c = 1. - x
+    c = (x * x / 4. + 1.) / c
+    d = (0.375 * x * x - 1.) * x
+    tu = s / (r * a * c)
+    y = tu
+    c = y + 1
+    while (np.abs (y - c) > EPS):
+ 
+        sy = np.sin(y)
+        cy = np.cos(y)
+        cz = np.cos(b + y)
+        e = 2. * cz * cz - 1.
+        c = y
+        x = e * cy
+        y = e + e - 1.
+        y = (((sy * sy * 4. - 3.) * y * cz * d / 6. + x) *
+              d / 4. - cz) * sy * d + tu
+ 
+    b = cu * cy * cf - su * sy
+    c = r * np.sqrt(sa * sa + b * b)
+    d = su * cy + cu * sy * cf
+    glat2 = (np.arctan2(d, c) + np.pi) % (2*np.pi) - np.pi
+    c = cu * cy - su * sy * cf
+    x = np.arctan2(sy * sf, c)
+    c = ((-3. * c2a + 4.) * f + 4.) * c2a * f / 16.
+    d = ((e * cy * c + cz) * sy * c + y) * sa
+    glon2 = ((glon1 + x - (1. - c) * d * f + np.pi) % (2*np.pi)) - np.pi    
+ 
+    baz = (np.arctan2(sa, b) + np.pi) % (2 * np.pi)
+ 
+    glon2 *= 180./np.pi
+    glat2 *= 180./np.pi
+    baz *= 180./np.pi
+ 
+    return (glon2, glat2, baz)
+
+# <codecell>
+
+def great(m, startlon, startlat, azimuth,*args, **kwargs):
+    """
+    function to draw great circle, takes into account crossing the border
+    by: Thomas Lecocq
+    """
+    glon1 = startlon
+    glat1 = startlat
+    glon2 = glon1
+    glat2 = glat1
+ 
+    step = 50
+ 
+    glon2, glat2, baz = shoot(glon1, glat1, azimuth, step)
+    if azimuth-180 >= 0:
+        while glon2 <= startlon:
+            m.drawgreatcircle(glon1, glat1, glon2, glat2,del_s=50,**kwargs)
+            azimuth = baz + 180.
+            glat1, glon1 = (glat2, glon2)
+ 
+            glon2, glat2, baz = shoot(glon1, glat1, azimuth, step)
+    elif azimuth-180 < 0:
+        while glon2 >= startlon:
+            m.drawgreatcircle(glon1, glat1, glon2, glat2,del_s=50,**kwargs)
+            azimuth = baz + 180.
+            glat1, glon1 = (glat2, glon2)
+ 
+            glon2, glat2, baz = shoot(glon1, glat1, azimuth, step
+
