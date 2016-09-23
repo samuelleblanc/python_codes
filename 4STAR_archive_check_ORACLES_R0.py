f0da427595e557957a9f4cacc4f6f0673cc0ed16
samuelleblanc <samuel.elie.leblanc@gmail.com>
2016-09-22 17:53:31 -0700

    Archive check of ORACLES AOD 4STAR R0

---
+++ 4STAR_archive_check_ORACLES_R0.py
@@ -0,0 +1,317 @@
+
+# coding: utf-8
+
+# # Intro
+# Simple Program to load and check the 4STAR archive files.
+# 
+# For R0 of ORACLES, aod and Gases
+
+# # Load the defaults and imports
+
+# In[1]:
+
+get_ipython().magic(u'config InlineBackend.rc = {}')
+import matplotlib 
+matplotlib.rc_file('C:\\Users\\sleblan2\\Research\\python_codes\\file.rc')
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import numpy as np
+import Sp_parameters as Sp
+from load_utils import mat2py_time, toutc, load_ict
+from Sp_parameters import smooth
+
+
+# In[ ]:
+
+from linfit import linfit
+
+
+# In[2]:
+
+get_ipython().magic(u'matplotlib notebook')
+
+
+# In[3]:
+
+fp ='C:/Users/sleblan2/Research/ORACLES/'
+
+
+# # load the files
+
+# In[218]:
+
+days = ['20160830','20160831','20160902','20160904','20160906','20160908','20160910','20160912','20160914','20160918']
+
+
+# In[202]:
+
+days = ['20160914','20160918']
+
+
+# In[219]:
+
+vv = 'R0'
+
+
+# In[220]:
+
+outaod_RA = []
+outaod_head_RA = []
+outgas_RA = []
+outgas_head_RA = []
+for d in days:
+    fname_aod = fp+'aod_ict/4STAR-AOD_P3_{}_{vv}.ict'.format(d,vv=vv)
+    tt,th = load_ict(fname_aod,return_header=True)
+    outaod_RA.append(tt)
+    outaod_head_RA.append(th)
+    
+    #fname_gas = fp+'gas_ict/korusaq-4STAR-GASES_DC8_{}_RA.ict'.format(d)
+    #ttr,thr = load_ict(fname_gas,return_header=True)
+    #outgas_RA.append(ttr)
+    #outgas_head_RA.append(thr)
+
+
+# ## Check the files for integrity and header info
+
+# In[221]:
+
+for i,s in enumerate(outaod_head_RA[0]):
+    for ig,g in enumerate(outaod_head_RA):
+        if not s==g[i]:
+            print 'no match on {vv} aod string line {}: {} and {vv} of num {}:{} '.format(i,s,ig,g[i],vv=vv)
+#    for ir,r in enumerate(outgas_head_RA):
+#        if not s==r[i]:
+#            print 'no match on RA gas string line {}: {} and RA of num {}:{} '.format(i,s,ir,r[i])
+
+
+# In[206]:
+
+print 'day:       AOD {vv}     GAS {vv}'.format(vv=vv)
+for i,d in enumerate(days):
+    try:
+        print '{}: {}  {}'.format(d,len(outaod_RA[i]['Start_UTC']),len(outgas_RA[i]['Start_UTC']))
+    except:
+        print '{}: missed'.format(d)
+
+
+# In[207]:
+
+outaod_head_RA[0]
+
+
+# In[9]:
+
+outgas_head_RA[0]
+
+
+# ## Check the variables in header
+
+# In[222]:
+
+nm = outaod_RA[0].dtype.names
+
+
+# In[223]:
+
+nm
+
+
+# In[224]:
+
+wl = nm[6:-1]
+
+
+# In[225]:
+
+plt.figure()
+plt.plot(out_R2[0][nm[0]],out_R2[0][nm[9]],'.')
+for x in out_R2[0][nm[0]][np.where(out_R2[0][nm[4]]==1)[0]]:
+    plt.axvline(x,color='#DDDDDD',alpha=0.02)
+
+
+# In[226]:
+
+for a in wl:
+    print a
+
+
+# # Plot the files
+
+# In[211]:
+
+for i,d in enumerate(days):
+    fig,ax = plt.subplots(2,sharex=True,figsize=(9,5))
+    ax = ax.ravel()
+    ax[0].set_title('AOD {} saved file for flight {}'.format(vv,d))
+    ax[0].set_color_cycle([plt.cm.gist_ncar(k) for k in np.linspace(0, 1, len(wl))])
+    for aod in wl:
+        ax[0].plot(outaod_RA[i][nm[0]],outaod_RA[i][aod],'.',label=aod)
+    try:
+        for x in outaod_RA[i][nm[0]][np.where(outaod_RA[i][nm[4]]==1)[0]]:
+            ax[0].axvline(x,color='#DDDDDD',alpha=0.02)
+    except:
+        pass
+    ax[0].set_ylabel('AOD')
+    ax[0].set_ylim(0,1)
+    ax[0].axhline(0,color='k')
+    box = ax[0].get_position()
+    ax[0].set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    ax[0].legend(frameon=False,loc='center left',bbox_to_anchor=(1.1,-0.2),numpoints=1)
+    ax[1].plot(outaod_RA[i][nm[0]],outaod_RA[i]['GPS_Alt'],'.')
+    ax[1].set_ylabel('Alt [m]')
+    axy = ax[1].twinx()
+    axy.plot(outaod_RA[i][nm[0]],outaod_RA[i]['amass_aer'],'.g')
+    axy.set_ylabel('Airmass factor',color='g')
+    box = ax[1].get_position()
+    ax[1].set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    box = axy.get_position()
+    axy.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    ax[1].set_xlabel('UTC [h]')
+    plt.savefig(fp+'aod_ict/{vv}_{}.png'.format(d,vv=vv),dpi=600,transparent=True)
+
+
+# ## Make plots of angstrom exponent
+
+# In[233]:
+
+def calc_angs(time,w,aod,flag):
+    'Program to calculate the angstrom exponent by fitting linearly on the aod'
+    ang = np.zeros_like(time)
+    for i,t in enumerate(time):
+        if not flag[i]==1:
+            c,cm = linfit(np.log10(w),-np.log10(aod[:,i]))
+            p = np.array([c[1],c[0]])
+            ang[i] = c[0] 
+        else:
+            ang[i] = np.nan
+        if (ang[i]<1.0) & (aod[2,i]>0.8):
+            ang[i] = np.nan
+    return ang
+
+
+# In[227]:
+
+wls = [0.38,0.452,0.501,0.520,0.532,0.55,0.606,0.620,0.675,0.781,0.865,1.02,1.04,1.064,1.236,1.559]
+
+
+# In[234]:
+
+aodrr, angarr = [],[]
+for i,d in enumerate(days):
+    aodarr = []
+    for w in wl:
+        aodarr.append(outaod_RA[i][w])
+    aodarr = np.array(aodarr)
+
+    ang = calc_angs(outaod_RA[i][nm[0]],wls,aodarr,outaod_RA[i][nm[4]])
+    aodrr.append(aodarr)
+    angarr.append(ang)
+
+
+# In[235]:
+
+for i,d in enumerate(days):
+    fig,ax = plt.subplots(1,sharex=True,figsize=(11,5))
+    ax.set_title('Angstrom profile {} for flight {}'.format(vv,d))
+    v = ax.scatter(outaod_RA[i]['Latitude'],outaod_RA[i]['GPS_Alt'],(20*outaod_RA[i][wl[2]])**3.0,c=angarr[i],
+                   marker='o',edgecolor='None',vmin=0.8,vmax=1.8)
+    ax.set_ylabel('Altitude [m]')
+    ax.set_ylim(0,7000)
+    ax.set_xlim(-23.5,-9.5)
+    ax.axhline(0,color='k')
+    ax.set_xlabel('Latitude [$^\\circ$]')
+   
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.95, box.height])
+    
+    cbaxes = fig.add_axes([0.9, 0.1, 0.02, 0.5]) 
+    cbar =  plt.colorbar(v,extend='both',cax=cbaxes)
+    cbar.set_label('Angstrom exponent')
+    
+    sizes = [0.02,0.2,0.4,0.6,0.8]
+    labels = ['$\\tau_{{0.5}}$={0}'.format(s) for s in sizes]
+    points = [ax.scatter([], [], s=(20*s)**3.0, c='None',marker='o',edgecolor='k') for s in sizes]
+    plt.legend(points, labels, scatterpoints=1,frameon=False,loc='upper left',bbox_to_anchor=(-1.2,1.75))
+    
+    plt.savefig(fp+'aod_ict/{vv}_{}_lat_alt_angstrom.png'.format(d,vv=vv),dpi=600,transparent=True)
+
+
+# ## make plots of the gases
+
+# In[ ]:
+
+for i,d in enumerate(days):
+    fig,ax = plt.subplots(2,sharex=True,figsize=(9,5))
+    ax = ax.ravel()
+    ax[0].set_title('Gas RA saved file for flight {}'.format(d))
+    ax[0].set_color_cycle([plt.cm.gist_ncar(k) for k in np.linspace(0, 1, len(wl))])
+    for aod in wl:
+        ax[0].plot(outgas_RA[i][nm[0]],outgas_RA[i][aod],'.',label=aod)
+    try:
+        for x in outgas_RA[i][nm[0]][np.where(outgas_RA[i][nm[4]]==1)[0]]:
+            ax[0].axvline(x,color='#DDDDDD',alpha=0.02)
+    except:
+        pass
+    ax[0].set_ylabel('AOD')
+    ax[0].set_ylim(0,3)
+    ax[0].axhline(0,color='k')
+    box = ax[0].get_position()
+    ax[0].set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    ax[0].legend(frameon=False,loc='center left',bbox_to_anchor=(1.1,-0.2),numpoints=1)
+    ax[1].plot(outaod_RA[i][nm[0]],outaod_RA[i]['GPS_Alt'],'.')
+    ax[1].set_ylabel('Alt [m]')
+    axy = ax[1].twinx()
+    axy.plot(outaod_RA[i][nm[0]],outaod_RA[i]['amass_aer'],'.g')
+    axy.set_ylabel('Airmass factor',color='g')
+    box = ax[1].get_position()
+    ax[1].set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    box = axy.get_position()
+    axy.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    ax[1].set_xlabel('UTC [h]')
+    plt.savefig(fp+'aod_ict/RA_{}.png'.format(d),dpi=600,transparent=True)
+
+
+# In[32]:
+
+outgas_RA[0].dtype.names
+
+
+# In[36]:
+
+for i,d in enumerate(days):
+    fig,ax = plt.subplots(7,sharex=True,figsize=(9,5))
+    ax = ax.ravel()
+    ax[0].set_title('Gas RA saved file for flight {}'.format(d))
+    
+    ax[0].plot(outgas_RA[i]['Start_UTC'],outgas_RA[i]['CWV'])
+    ax[0].set_ylabel('CWV')
+    #ax[0].set_ylim(0,3)
+    ax[0].axhline(0,color='k')
+    #box = ax[0].get_position()
+    #ax[0].set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    #ax[0].legend(frameon=False,loc='center left',bbox_to_anchor=(1.1,-0.2),numpoints=1)
+    ax[1].plot(outgas_RA[i]['Start_UTC'],outgas_RA[i]['QA_CWV'],'x')
+    ax[1].set_ylabel('QA_CWV')
+    
+    ax[2].plot(outgas_RA[i]['Start_UTC'],outgas_RA[i]['VCD_O3'])
+    ax[2].set_ylabel('VCD_O3')
+    
+    ax[3].plot(outgas_RA[i]['Start_UTC'],outgas_RA[i]['QA_O3'],'x')
+    ax[3].set_ylabel('QA_O3')
+    
+    ax[4].plot(outgas_RA[i]['Start_UTC'],outgas_RA[i]['VCD_NO2'])
+    ax[4].set_ylabel('VCD_NO2')
+    
+    ax[5].plot(outgas_RA[i]['Start_UTC'],outgas_RA[i]['QA_NO2'],'x')
+    ax[5].set_ylabel('QA_NO2')
+    
+    ax[6].plot(outgas_RA[i]['Start_UTC'],outgas_RA[i]['qual_flag'],'x')
+    ax[6].set_ylabel('qual_flag')
+    
+    ax[6].set_xlabel('UTC [h]')
+
+
+# In[ ]:
+
+
+
