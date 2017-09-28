105e5fb2f68f38ea7741f48a3292963feef28f67
samuelleblanc <samuel.elie.leblanc@gmail.com>
2017-09-28 15:55:21 -0300

    Start of a get_flight_data program

--- Get_flight_data.py
+++ Get_flight_data.py
@@ -1,24 +1,24 @@
 
 # coding: utf-8
 
-# In[8]:
+# In[48]:
 
 import matplotlib.pyplot as plt
 
 
-# In[9]:
+# In[1]:
 
 get_ipython().magic(u'matplotlib notebook')
 
 
-# In[10]:
+# In[2]:
 
 fp = 'C:\Users\NASA_\Documents\Research\ORACLES\plots\\'
 
 
-# In[11]:
+# In[58]:
 
-def init_packets(name):
+def init_packets(name, ip='10.15.1.1',plane='N426NA'):
     """
     Function to load the inital set of packets from the data API
     """
@@ -29,7 +29,7 @@ def init_packets(name):
     toutc = lambda x: x.hour+x.minute/60.0+x.second/3600.0
     
     #Get the names of parameters to init dict
-    site = 'http://10.15.1.1/API/parameter_data/N426NA/{}/XML'.format(name)
+    site = 'http://{ip}/API/parameter_data/{p}/{m}/XML'.format(ip=ip,m=name,p=plane)
     r = urlopen(site)
     rr = r.read()
     rn = [s[s.rfind('<point')+len('<point')+1:s.find('/>')] for s in rr.splitlines() if s.find('point')>-1]
@@ -56,12 +56,14 @@ def init_packets(name):
                     d[n] = np.array([float(dict_vals[i])])
                 except:
                     d[n] = np.array([dict_vals[i]])
+    d['ip'] = ip
+    d['plane'] = plane
     return d
 
 
-# In[12]:
+# In[59]:
 
-def get_packets(d,start_time=None,end_time=None,verbose=True):
+def get_packets(d,start_time=None,end_time=None,verbose=True,ip='10.15.1.1',plane='N426NA'):
     """
     Get packets from the start_time (defaults to 0) in seconds from 1970
     appends the packets to the correct values in the dict, and returns the dict
@@ -71,13 +73,24 @@ def get_packets(d,start_time=None,end_time=None,verbose=True):
     from urllib2 import urlopen
     
     # get the packets from web using the XML interface
-    name = d['header'][0]
+    try:
+        name = d['header'][0]
+    except:
+        print d.keys()
+    try:
+        ip = d['ip']
+    except:
+        pass
+    try:
+        plane = d['plane']
+    except:
+        pass
     last = d['timestamp'][-1]
     if len(d['header'])>1:
         epoch_time = datetime(1970,1,1)
         start_time = (last.toordinal()-epoch_time.toordinal())*24*60*60+last.hour*3600+last.minute*60+last.second
     
-    site = 'http://10.15.1.1/API/parameter_data/N426NA/{}/XML?Start={}'.format(name,start_time)
+    site = 'http://{ip}/API/parameter_data/{p}/{m}/XML?Start={s}'.format(m=name,s=start_time,ip=ip,p=plane)
     if end_time:
         site = site+'&End={}'.format(end_time)
     r = urlopen(site)
@@ -85,6 +98,8 @@ def get_packets(d,start_time=None,end_time=None,verbose=True):
     rn = [s[s.rfind('<point')+len('<point')+1:s.find('/>')] for s in rr.splitlines() if s.find('point')>-1]
     nn = float(len(rn))
     if verbose: print 'number of {} points found: {}'.format(name,nn)
+    if nn==0:
+        return d
     toutc = lambda x: x.hour+x.minute/60.0+x.second/3600.0
     for j,rnn in enumerate(rn):
         dict_names = [s.split('="')[0] for s in rnn.split('" ')]
@@ -111,13 +126,13 @@ def get_packets(d,start_time=None,end_time=None,verbose=True):
     return d
 
 
-# In[13]:
+# In[41]:
 
-def startup_packet(name):
+def startup_packet(name,ip='10.15.1.1',plane='N426NA'):
     'function to run at startup for slowly loading up the data structures, to prohibit overwhelming the data systems'
     from datetime import datetime
     
-    d = init_packets(name)
+    d = init_packets(name,ip=ip,plane=plane)
     
     # Try and loop a few times to get the right start time and load slowly
     t = datetime.now()
@@ -126,22 +141,87 @@ def startup_packet(name):
     now_time = (t.toordinal()-epoch_time.toordinal())*24*60*60+t.hour*3600+t.minute*60+t.second
     for i in xrange(48):
         end_time = start_time+3600/2
-        d = get_packets(d,start_time=start_time,end_time=end_time)
+        d = get_packets(d,start_time=start_time,end_time=end_time,ip=ip,plane=plane)
         if start_time>now_time:
             break
         else:
             start_time = end_time
+    d['stop_update'] = False
     return d
 
 
-# In[15]:
+# In[145]:
 
-iwg = startup_packet('IWG1')
+def update_packets(d):
+    'Function to be used in the threading module to update the data dict with incoming packets'
+    import time
+    from datetime import datetime
+    i=0
+    ee = 0
+    d['stop_update'] = False
+    while True:
+        if d.get('stop_update',False):
+            break
+        time.sleep(1)
+        t = datetime.now()
+        epoch_time = datetime(1970,1,1)
+        today_time = (t.toordinal()-epoch_time.toordinal())*24*60*60
+        now_time = today_time+t.hour*3600+t.minute*60+t.second
+        start_time = today_time+d['timestamp'][-1].hour*3600+d['timestamp'][-1].minute*60+d['timestamp'][-1].second
+        try:
+            d = get_packets(d,start_time=start_time)
+        except:
+            if ee>600:
+                print 'Time out, no more packets'
+                break
+            else:
+                ee = ee+1
+        i=i+1
+        #print i
+        #if i>11:
+        #    d['stop_update']=True
+    return d
 
 
-# In[16]:
+# In[82]:
 
-iwg = coma
+import threading
+help(threading.Thread)
+
+
+# In[146]:
+
+th = threading.Thread(target=update_packets,args=([s]))
+
+
+# In[ ]:
+
+th.run()
+
+
+# In[ ]:
+
+s['stop_update']
+
+
+# In[ ]:
+
+s['UTC'][-1]
+
+
+# In[20]:
+
+iwg = startup_packet('IWG1',ip='10.16.1.1',plane='N439NA')
+
+
+# In[44]:
+
+s = startup_packet('4STARB',ip='10.16.1.1',plane='N439NA')
+
+
+# In[50]:
+
+s = get_packets(s,ip='10.16.1.1',plane='N439NA')
 
 
 # In[80]:
@@ -164,10 +244,25 @@ higear.keys()
 iwg.keys()
 
 
-# In[86]:
+# In[46]:
+
+s.keys()
+
+
+# In[55]:
+
+import threading
+
+
+# In[57]:
+
+help(threading)
+
+
+# In[51]:
 
 plt.figure()
-plt.plot(iwg['r'],higear['cn'],'.')
+po = plt.plot(s['UTC'],s['nm501'],'.')
 
 
 # In[83]:
