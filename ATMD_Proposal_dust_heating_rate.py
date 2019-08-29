b6584433ea74da3ebf8b1aa8e6e9e674031115a0
Samuel LeBlanc <samuelleblanc@users.noreply.github.com>
2019-08-29 00:19:00 -0700

    Progress on heating rate from dust

--- ATMD_Proposal_dust_heating_rate.py
+++ ATMD_Proposal_dust_heating_rate.py
@@ -82,42 +82,128 @@ fp =getpath('ORACLES')
 
 # ## Load the SSFR files
 
-# In[10]:
+# In[4]:
 
 
 ssfr = sio.idl.readsav(fp+'data_other_2018/SSFR/for_Sam_20181025_SSFR.out')
 
 
-# In[11]:
+# In[5]:
 
 
 ssfr.keys()
 
 
+# Now interpolate the nadir spectra to the zenith wavelengths
+
+# In[19]:
+
+
+def interp_spline(xold, yold, ynew):
+    uv = UnivariateSpline(xold,yold,ext=0,k=1)
+    return uv(ynew)
+
+
+# In[21]:
+
+
+ssfr['nadspectra'].shape
+
+
+# In[25]:
+
+
+ssfr['nadlambda'][0:10]
+
+
+# In[26]:
+
+
+ssfr['nspectra'] = np.array([interp_spline(ssfr['nadlambda'],ssfr['nadspectra'][i,:],ssfr['zenlambda']) for i in xrange(len(ssfr['utc']))])
+
+
+# In[27]:
+
+
+ssfr['nspectra'].shape
+
+
+# In[28]:
+
+
+ssfr['f_net'] = ssfr['zenspectra'] - ssfr['nadspectra']
+
+
 # ## Load the 4STAR file
 
-# In[14]:
+# In[6]:
 
 
 s = lu.load_ict(fp+'aod_ict_2018/4STAR-AOD_P3_20181025_R1.ict')
 
 
-# In[17]:
+# In[7]:
 
 
 sp = sio.loadmat(fp+'data_2018/4STAR_20181025starsun.mat')
 
 
-# # Run analysis and prepare variables
+# ## Load the merge file
+
+# In[13]:
+
+
+mrg,mrg_head = lu.load_netcdf(fp+'data_other_2018/mrg1_P3_20181025_R13.nc',everything=True)
+
+
+# # Build up the plots to select the region
 # Do some of the calculations to the data here
 
 # Get the location of the dust profile
 
-# In[21]:
+# In[14]:
 
 
 plt.figure()
 plt.plot(s['Start_UTC'],s['GPS_Alt'],'.')
+
+
+# In[10]:
+
+
+plt.figure()
+plt.plot(s['Latitude'],s['GPS_Alt'],'.')
+
+
+# In[15]:
+
+
+pfl = [13.565, 13.95]
+
+
+# In[17]:
+
+
+i_ssfr = (ssfr['utc']>pfl[0]) & (ssfr['utc']<pfl[1])
+
+
+# In[30]:
+
+
+ssfr['alt'] = interp_spline(s['Start_UTC'],s['GPS_Alt'],ssfr['utc'])
+
+
+# In[33]:
+
+
+plt.figure()
+plt.pcolor(ssfr['zenlambda'],ssfr['alt'][i_ssfr],ssfr['f_net'][i_ssfr,:])
+
+
+# In[36]:
+
+
+ssfr['alt'][i_ssfr]
 
 
 # # Plotting
