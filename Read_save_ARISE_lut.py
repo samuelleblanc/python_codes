55541bbbc9c6a36c1f759fcee10a2c746a43fe56
Samuel LeBlanc <samuelleblanc@users.noreply.github.com>
2019-10-12 15:43:31 -0700

    ARISE lut reader update

--- Read_save_ARISE_lut.py
+++ Read_save_ARISE_lut.py
@@ -73,12 +73,12 @@ else:
 
 
 vv = 'v3'
-mu = np.arange(2.7,4.0,0.1)
-sza = np.round(np.arccos(1.0/mu)*180.0/np.pi)
+mu = np.arange(3.0,4.0,0.1)
+sza = np.arccos(1.0/mu)*180.0/np.pi
 tau = np.array([0.1,0.2,0.3,0.5,0.75,1.0,1.25,1.5,1.75,2.0,2.3,2.6,3.0,3.5,4.0,4.5,5.0,
-       6.0,7.0,8.0,9.0,10.0,12.5,15.0,17.5,20.0,22.0,25.0,27.0,30.0,35.0,40.0,50.0])
-ref = np.append(np.append(np.arange(2,15),np.arange(15,30,2)),np.ceil(np.arange(30,61,3.0)))
-zout = [0.2,2.0,100.0]
+       6.0,7.0,8.0,9.0,10.0,12.5,15.0,17.5,20.0,22.0,25.0,27.0,30.0,35.0,40.0,50.0,60.0])
+ref = np.append(np.append(np.arange(2,15),np.arange(15,30,2)),np.ceil(np.arange(30,61,5.0)))
+zout = [0.1,2.0,100.0]
 
 
 # In[7]:
@@ -92,7 +92,7 @@ fp_out = os.path.join(fp_rtm,'output','%s_ARISE'%vv)
 
 dat = RL.read_lut(fp_out,zout=zout,tau=tau,ref=ref,sza=sza,
                   phase=['wc','ic'],
-                  fmt='lut_sza{sza:02.0f}_tau{tau:06.2f}_ref{ref:04.1f}_{phase}_w{iwvl:1d}.dat',
+                  fmt='lut_sza{sza:04.1f}_tau{tau:06.2f}_ref{ref:04.1f}_{phase}_w{iwvl:1d}.dat',
                   split_wvl=True)
 
 
