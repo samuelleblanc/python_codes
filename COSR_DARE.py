9cd34a5aa73b2a32b66b46a646d236f79db83801
Samuel LeBlanc <samuelleblanc@users.noreply.github.com>
2019-10-21 13:20:36 -0700

    Some advancement on DARE for COSR

--- COSR_DARE.py
+++ COSR_DARE.py
@@ -24,7 +24,7 @@
 #   
 # Dependencies:
 # 
-#     - load_utils.py : for loading OMI HDF5 files
+#     - load_utils.py
 #     - matplotlib
 #     - numpy
 #   
@@ -40,42 +40,164 @@
 
 # # Prepare python environment
 
-# In[1]:
+# In[13]:
 
 
-get_ipython().magic(u'config InlineBackend.rc = {}')
-import matplotlib 
-import os
-matplotlib.rc_file(os.path.join(os.getcwd(),'file.rc'))
-import matplotlib.pyplot as plt
-import matplotlib.colors as colors
 import numpy as np
 import Sp_parameters as Sp
-from load_utils import mat2py_time, toutc, load_ict
 import load_utils as lu
-import plotting_utils as pu
 from path_utils import getpath
 import hdf5storage as hs
+import scipy.io as sio
 from datetime import datetime
 from scipy.interpolate import UnivariateSpline
-import matplotlib.dates as mdates
-from mpl_toolkits.basemap import Basemap
-import scipy.stats as st
+
+
+# In[29]:
+
+
+import pandas as pd
 
 
 # In[2]:
 
 
+import matplotlib.pyplot as plt
 get_ipython().magic(u'matplotlib notebook')
 
 
-# In[3]:
+# In[31]:
 
 
-fp =getpath('ORACLES')
+name = 'COSR'
+vv = 'v1'
+
+
+# In[5]:
+
+
+fp =getpath('COSR')
+fp_rtm = '/nobackup/sleblan2/rtm/'
+fp_uvspec = '/u/sleblan2/libradtran/libRadtran-2.0-beta/bin/uvspec'
+fp_rtmdat = '/nobackup/sleblan2/COSR/rtm/' #'/u/sleblan2/4STAR/rtm_dat/'
+
+
+# ## Setup command line interface
+
+# In[6]:
+
+
+import argparse
+
+
+# In[7]:
+
+
+long_description = """    Prepare or save the direct Aerosol radiative effect files for calculations. """
+
+
+# In[32]:
+
+
+parser = argparse.ArgumentParser(description=long_description)
+parser.add_argument('-doread','--doread',help='if set, will only read the output, not produce them',
+                    action='store_true')
+parser.add_argument('-d','--daystr',nargs='?',help='The day string (yyyymmdd) for the desired flight data. Defaults to 20180624')
+
+
+# In[9]:
+
+
+in_ = vars(parser.parse_args())
+do_read = in_.get('doread',False)
+day = in_.get('daystr','20180624')
 
 
 # # Load files
+
+# ## Load the in situ files
+
+# In[14]:
+
+
+neph = sio.loadmat(fp+'20180624_nephclap.csv_20191004_152550.mat.mat')
+
+
+# In[15]:
+
+
+neph.keys()
+
+
+# In[28]:
+
+
+neph['None'][0][4]
+
+
+# In[34]:
+
+
+situ = pd.read_csv(fp+'{}_neph_clap.csv'.format(day))
+
+
+# In[36]:
+
+
+situ
+
+
+# In[44]:
+
+
+insitu = situ.to_dict('list')
+
+
+# In[46]:
+
+
+insitu.keys()
+
+
+# In[56]:
+
+
+[insitu[k]=np.array(insitu[k]) for k in insitu.keys()]
+
+
+# In[47]:
+
+
+insitu['ssa_500nm'] = np.array(insitu['totScatCalc_500nm'])/np.array(insitu['extCalc500nm'])
+
+
+# In[50]:
+
+
+import Sp_parameters as Sp
+
+
+# In[55]:
+
+
+plt.figure()
+plt.plot(insitu['ssa_500nm'],'.')
+plt.plot(Sp.smooth(insitu['ssa_500nm'],60,old=True),'-r')
+
+
+# ## Load the skyscan results
+
+# In[17]:
+
+
+sky = sio.loadmat(fp+'4STAR_20180624_135_SKYP.created_20190329_003621.ppl_lv15.mat')
+
+
+# In[18]:
+
+
+sky.keys()
+
 
 # # Run analysis and prepare variables
 # Do some of the calculations to the data here
