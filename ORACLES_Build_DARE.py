6012c3058fbd77e032cbc56b2e1265832bc47a93
Samuel LeBlanc <samuel.elie.leblanc@gmail.com>
2020-01-06 16:27:19 -0500

    New DARE calculations based on updated retrievals

--- ORACLES_Build_DARE.py
+++ ORACLES_Build_DARE.py
@@ -42,7 +42,7 @@
 
 # # Import of modules
 
-# In[128]:
+# In[1]:
 
 
 import numpy as np
@@ -62,7 +62,7 @@ from write_utils import nearest_neighbor, iterate_dict_unicode
 get_ipython().magic(u'matplotlib notebook')
 
 
-# In[86]:
+# In[3]:
 
 
 from tqdm.notebook import tqdm 
@@ -84,11 +84,11 @@ name = 'ORACLES'
 # In[6]:
 
 
-vv = 'v1'
+vv = 'v2'
 vr = 'R3'
 
 
-# In[15]:
+# In[7]:
 
 
 fp = getpath(name)
@@ -103,77 +103,95 @@ fp_rtmdat = fp_rtm+'dat/'
 
 # ## Load the 4STAR AOD
 
-# In[16]:
+# In[8]:
 
 
 ar = hs.loadmat(fp+'/aod_ict/v8/{v}/all_aod_ict_{v}_2016.mat'.format(v=vr))
 
 
-# In[17]:
+# In[9]:
 
 
 ar.keys()
 
 
-# In[18]:
+# In[10]:
 
 
 ar['AOD0501'].shape
 
 
-# In[19]:
+# In[11]:
 
 
 sza = np.arccos(1.0/ar['amass_aer'])*180.0/np.pi
 
 
-# In[20]:
+# In[12]:
 
 
 days = ['20160824','20160825','20160827','20160830','20160831','20160902','20160904','20160906','20160908',
        '20160910','20160912','20160914','20160918','20160920','20160924','20160925','20160927','20160930']
 
 
+# In[123]:
+
+
+len(days)
+
+
+# In[122]:
+
+
+ar['days']
+
+
 # ## Load the retrieved Cloud properties
 
-# In[21]:
+# In[13]:
 
 
-cl = hs.loadmat(fp+'data_other/ssfr_2016_retrieved_COD.mat')
+cl = hs.loadmat(fp+'data_other/ssfr_2016_retrieved_COD_{}.mat'.format(vv))
 
 
-# In[22]:
+# In[14]:
 
 
 cl.keys()
 
 
-# In[23]:
+# In[15]:
 
 
 cl['tau'].shape
 
 
-# In[24]:
+# In[16]:
 
 
 dds = ['20160830','20160831','20160902','20160904','20160906','20160908',
        '20160910','20160912','20160914','20160918','20160920','20160924','20160925','20160927']
 
 
-# In[25]:
+# In[124]:
+
+
+len(dds)
+
+
+# In[125]:
 
 
 cl['days']
 
 
-# In[26]:
+# In[18]:
 
 
 dd = np.unique(cl['days'])
 
 
-# In[27]:
+# In[128]:
 
 
 cod,ref = [],[]
@@ -188,19 +206,19 @@ for d in dd:
     ref = np.append(ref,ref_tmp)
 
 
-# In[28]:
+# In[129]:
 
 
 cod.shape
 
 
-# In[29]:
+# In[130]:
 
 
 len(dds)
 
 
-# In[30]:
+# In[131]:
 
 
 len(np.unique(ar['days']))
@@ -208,7 +226,7 @@ len(np.unique(ar['days']))
 
 # ## Load the skyscan retrievals
 
-# In[52]:
+# In[23]:
 
 
 try:
@@ -222,7 +240,7 @@ except:
     ae = ae5
 
 
-# In[80]:
+# In[24]:
 
 
 ke = ae.keys()
@@ -230,62 +248,62 @@ ke.sort()
 ke
 
 
-# In[81]:
+# In[192]:
 
 
 ae['AOD_meas'][0]
 
 
-# In[33]:
+# In[26]:
 
 
 ae_dict['SSA']
 
 
-# In[82]:
+# In[27]:
 
 
 ae['SSA'].shape
 
 
-# In[194]:
+# In[28]:
 
 
 ae_dict['time']
 
 
-# In[83]:
+# In[29]:
 
 
 ae['time']/3600.0
 
 
-# In[84]:
+# In[30]:
 
 
 days = ['20160824','20160825','20160827','20160830','20160831','20160902','20160904','20160906','20160908',
        '20160910','20160912','20160914','20160918','20160920','20160924','20160925','20160927','20160930']
 
 
-# In[87]:
+# In[31]:
 
 
 ar['doy'] = np.array([datetime.strptime(days[int(d)],'%Y%m%d').timetuple().tm_yday for d in ar['days']])
 
 
-# In[88]:
+# In[32]:
 
 
 datetime.strptime(days[4],'%Y%m%d').timetuple().tm_yday
 
 
-# In[89]:
+# In[33]:
 
 
 ar['time_ae'] = ar['Start_UTC']+(24.0*(ar['doy']-244))
 
 
-# In[90]:
+# In[34]:
 
 
 ar['time_ae']
@@ -293,21 +311,21 @@ ar['time_ae']
 
 # # Prepare the base dict and defaults
 
-# In[91]:
+# In[35]:
 
 
 from datetime import datetime
 datetime(2015,11,17).timetuple().tm_yday
 
 
-# In[92]:
+# In[120]:
 
 
 # for all 4STAR aerosol arrays
-fla = (ar['flag_acaod']==1) & ar['fl'] & ar['fl_QA'] & (ar['days']>2.0)
+fla = (ar['flag_acaod']==1) & ar['fl'] & ar['fl_QA'] & (ar['days']>2.0) 
 
 
-# In[93]:
+# In[121]:
 
 
 # for the cod and ref arrays
@@ -315,7 +333,25 @@ fld = (ar['days']>2.0) & (ar['days']!=17.0)
 flb = (ar['flag_acaod'][fld]==1) & ar['fl'][fld] & ar['fl_QA'][fld]
 
 
-# In[94]:
+# In[137]:
+
+
+len(ar['AOD0355'][fla])
+
+
+# In[138]:
+
+
+len(cod[flb])
+
+
+# In[145]:
+
+
+sum(np.isfinite(cod[~flb])),sum(np.isfinite(cod[flb])),len(cod[flb])
+
+
+# In[38]:
 
 
 ka = ar.keys()
@@ -323,19 +359,19 @@ ka.sort()
 ka
 
 
-# In[95]:
+# In[146]:
 
 
 doy = datetime.strptime(dds[int(ar['days'][fla][0])],'%Y%m%d').timetuple().tm_yday
 
 
-# In[96]:
+# In[147]:
 
 
 doy
 
 
-# In[112]:
+# In[148]:
 
 
 geo = {'lat':ar['Latitude'][0],'lon':ar['Longitude'][0],'doy':doy,'zout':[0,1.5,100.0]}
@@ -346,7 +382,7 @@ source = {'wvl_range':[201.0,4900.0],'source':'solar','integrate_values':True,'r
 albedo = {'create_albedo_file':False,'sea_surface_albedo':True,'wind_speed':5.0}
 
 
-# In[113]:
+# In[149]:
 
 
 cloud['phase'] = 'wc'
@@ -357,27 +393,27 @@ pmom = Rl.make_pmom_inputs(fp_rtm=fp_rtmdat,source='solar',deltascale=False)
 cloud['moms_dict'] = pmom
 
 
-# In[114]:
+# In[150]:
 
 
 pmom['wvl'][0] = 0.250
 
 
-# In[115]:
+# In[151]:
 
 
 wvl = np.append(np.append([250.0],ae['wavelength']),4900.0)
 wvl
 
 
-# In[116]:
+# In[152]:
 
 
 aero = {'expand_hg':True,'disort_phase':False,'z_arr':[2.0,5.0],
         'wvl_arr':wvl}
 
 
-# In[117]:
+# In[153]:
 
 
 def fx_aero(aprop):
@@ -386,7 +422,7 @@ def fx_aero(aprop):
     return np.array([atmp,atmp])
 
 
-# In[118]:
+# In[154]:
 
 
 def fx_ext(a0,a1,a2,wvl=wvl):
@@ -396,19 +432,19 @@ def fx_ext(a0,a1,a2,wvl=wvl):
     return np.array([aod/3.0,aod*0.0])
 
 
-# In[119]:
+# In[155]:
 
 
 aero['ext'] = fx_ext(ar['AOD_polycoef_a0'][fla][0],ar['AOD_polycoef_a1'][fla][0],ar['AOD_polycoef_a2'][fla][0])
 
 
-# In[120]:
+# In[156]:
 
 
 aero['asy'] = fx_aero(ae['g_total'][0])
 
 
-# In[121]:
+# In[157]:
 
 
 aero['ssa'] = fx_aero(ae['SSA'][0])
@@ -416,7 +452,7 @@ aero['ssa'] = fx_aero(ae['SSA'][0])
 
 # ## Prepare the file list and saving
 
-# In[122]:
+# In[158]:
 
 
 def isjupyter():
@@ -432,7 +468,11 @@ def isjupyter():
         return False      # Probably standard Python interpreter
 
 
-# In[123]:
+# ## Write the files
+
+# ### Conventional
+
+# In[87]:
 
 
 # open the list file
@@ -441,7 +481,7 @@ fpp_in = fp_rtm+'input/{}_DARE_{}/'.format(name,vv)
 fpp_out = fp_rtm+'output/{}_DARE_{}/'.format(name,vv)
 
 
-# In[124]:
+# In[88]:
 
 
 if not os.path.isdir(fpp_in):
@@ -450,15 +490,13 @@ if not os.path.isdir(fpp_out):
      os.mkdir(fpp_out)
 
 
-# ## Write the files
-
 # In[120]:
 
 
 # for writing out the files
 
 
-# In[125]:
+# In[89]:
 
 
 if isjupyter():
@@ -480,7 +518,7 @@ for i,u in enumerate(ar['Start_UTC'][fla]):
     if abs(ar['time_ae'][fla][i]-ae['time']/3600.0)[iae]<1.0: 
 
         aero['ext'] = fx_ext(ar['AOD_polycoef_a0'][fla][i],ar['AOD_polycoef_a1'][fla][i],ar['AOD_polycoef_a2'][fla][i])
-        aero['ssa'] = fx_aero(ae['SSA'][0])
+        aero['ssa'] = fx_aero(ae['SSA'][iae])
         aero['asy'] = fx_aero(ae['g_total'][iae])
 
         Rl.write_input_aac(fpp_in+f_in,geo=geo,aero=aero,cloud=cloud,source=source,albedo=albedo,
@@ -500,56 +538,250 @@ for i,u in enumerate(ar['Start_UTC'][fla]):
 f.close()
 
 
+# ### Multiprocessing
+
+# In[159]:
+
+
+from multiprocessing import Pool, cpu_count
+
+
+# In[160]:
+
+
+from copy import deepcopy
+import signal
+
+
+# In[161]:
+
+
+import warnings
+warnings.simplefilter('ignore')
+
+
+# In[162]:
+
+
+def worker_init():
+    # ignore the SIGINI in sub process, just print a log
+    def sig_int(signal_num, frame):
+        if verbose: 
+            print 'signal: %s' % signal_num
+        raise IOError
+    signal.signal(signal.SIGINT, sig_int)
+
+
+# In[163]:
+
+
+# open the list file
+f = open(fp_rtm+'{}_DARE_{}.sh'.format(name,vv),'w')
+fpp_in = fp_rtm+'input/{}_DARE_{}/'.format(name,vv)
+fpp_out = fp_rtm+'output/{}_DARE_{}/'.format(name,vv)
+
+
+# In[164]:
+
+
+if not os.path.isdir(fpp_in):
+    os.mkdir(fpp_in)
+if not os.path.isdir(fpp_out):
+     os.mkdir(fpp_out)
+
+
+# In[168]:
+
+
+if isjupyter():
+    pbar = tqdm(total=len(ar['Start_UTC'][fla]))
+bb = []
+for i,u in enumerate(ar['Start_UTC'][fla]):
+    
+    f_in = '{name}_{vv}_DARE_{i:03d}_withaero.dat'.format(name=name,vv=vv,i=i)
+
+    geo['lat'],geo['lon'],geo['sza'] = ar['Latitude'][fla][i],ar['Longitude'][fla][i],sza[fla][i]
+    day = days[ar['days'][fla][i].astype(int)]
+    geo['doy'] = datetime(int(day[0:4]),int(day[4:6]),int(day[6:])).timetuple().tm_yday
+
+    if ~np.isfinite(cod[flb][i]):
+        if isjupyter():
+            pbar.update(1)
+        continue
+    cloud['tau'],cloud['ref'] = cod[flb][i],ref[flb][i]
+    cloud['write_moments_file'] = True
+
+    iae = np.argmin(abs(ar['time_ae'][fla][i]-ae['time']/3600.0))
+
+    # Only run for aerosol rertievals within 1 hour
+    if abs(ar['time_ae'][fla][i]-ae['time']/3600.0)[iae]<1.0: 
+
+        aero['ext'] = fx_ext(ar['AOD_polycoef_a0'][fla][i],ar['AOD_polycoef_a1'][fla][i],ar['AOD_polycoef_a2'][fla][i])
+        aero['ssa'] = fx_aero(ae['SSA'][iae])
+        aero['asy'] = fx_aero(ae['g_total'][iae])
+
+        #Rl.write_input_aac(fpp_in+f_in,geo=geo,aero=aero,cloud=cloud,source=source,albedo=albedo,
+        #                           verbose=False,make_base=False,set_quiet=True)
+        f.write('{uv} < {fin} > {out}\n'.format(uv=fp_uvspec,fin=fpp_in+f_in,out=fpp_out+f_in))
+
+        f_in_noa = '{name}_{vv}_star_{i:03d}_noaero.dat'.format(name=name,vv=vv,i=i)
+        #Rl.write_input_aac(fpp_in+f_in,geo=geo,aero=aero_no,cloud=cloud,source=source,albedo=albedo,
+        #                           verbose=False,make_base=False,set_quiet=True)
+        f.write('{uv} < {fin} > {out}\n'.format(uv=fp_uvspec,fin=fpp_in+f_in_noa,out=fpp_out+f_in_noa))
+        
+        bb.append({'geo':deepcopy(geo),'cod':cod[flb][i],'ref':ref[flb][i],'aero':deepcopy(aero),
+                   'f_in':deepcopy(f_in),'f_in_noa':deepcopy(f_in_noa)})
+
+    if isjupyter(): 
+        pbar.update(1)
+    else:
+        print i
+
+f.close()
+
+
+# In[169]:
+
+
+def write_files(d,cloud=cloud,source=source,albedo=albedo,aero_no=aero_no):
+    'function to feed the pool of workers to write out the all the files'
+    cloud['tau'],cloud['ref'] = d['cod'],d['ref']
+    Rl.write_input_aac(fpp_in+d['f_in'],geo=d['geo'],aero=d['aero'],cloud=cloud,source=source,albedo=albedo,
+                                   verbose=False,make_base=False,set_quiet=True)
+    Rl.write_input_aac(fpp_in+d['f_in_noa'],geo=d['geo'],aero=aero_no,cloud=cloud,source=source,albedo=albedo,
+                                   verbose=False,make_base=False,set_quiet=True)
+
+
+# In[170]:
+
+
+p = Pool(cpu_count()-1,worker_init)
+
+
+# In[171]:
+
+
+len(bb)
+
+
+# In[174]:
+
+
+results = p.map(write_files,bb)
+
+
+# ## Run the calculations
+
 # Run the files from command line:
 # 
 # using command parallel --jobs=20 < ORACLES_DARE_v1.sh
 
-# In[129]:
+# In[175]:
 
 
 f_list = fp_rtm+'{}_DARE_{}.sh'.format(name,vv)
 
 
-# In[130]:
+# In[176]:
 
 
 get_ipython().system(u' wc -l $f_list')
 
 
-# In[ ]:
+# In[179]:
 
 
-get_ipython().system(u'parallel --job=20 --bar < $f_list')
+f_listout = f_list+'.out'
+
+
+# In[191]:
+
+
+get_ipython().system(u'parallel --jobs=7 --bar < $f_list 2> $f_listout')
 
 
 # ## Read the files
 
-# In[133]:
+# In[195]:
+
+
+fpp_out,name,vv,geo['zout']
+
+
+# In[198]:
 
 
 n = len(ar['Start_UTC'][fla])
 nz = len(geo['zout'])
 nw = len(aero['wvl_arr'])
-
-if isjupyter():
-    pbar = tqdm(total=n)
-    
 dat = {'cod':np.zeros(n)+np.nan,'ref':np.zeros(n)+np.nan,'ext':np.zeros((n,nw))+np.nan,
        'ssa':np.zeros((n,nw))+np.nan,'asy':np.zeros((n,nw))+np.nan,'zout':geo['zout'],
-       'wvl':aero['wvl_arr'],
+       'wvl':aero['wvl_arr'],'sza':np.zeros(n)+np.nan,
        'dn':np.zeros((n,nz))+np.nan,'up':np.zeros((n,nz))+np.nan,
        'dn_noa':np.zeros((n,nz))+np.nan,'up_noa':np.zeros((n,nz))+np.nan}
+
 for i,u in enumerate(ar['Start_UTC'][fla]):
     
     dat['cod'][i] = cod[flb][i]
     dat['ref'][i] = ref[flb][i]
+    dat['sza'][i] = sza[fla][i]
 
     iae = np.argmin(abs(ar['time_ae'][fla][i]-ae['time']/3600.0))
     # Only run for aerosol rertievals within 1 hour
     if abs(ar['time_ae'][fla][i]-ae['time']/3600.0)[iae]<1.0: 
 
         dat['ext'][i,:] = fx_ext(ar['AOD_polycoef_a0'][fla][i],ar['AOD_polycoef_a1'][fla][i],ar['AOD_polycoef_a2'][fla][i])[0]
-        dat['ssa'][i,:] = fx_aero(ae['SSA'][0])[0]
+        dat['ssa'][i,:] = fx_aero(ae['SSA'][iae])[0]
+        dat['asy'][i,:] = fx_aero(ae['g_total'][iae])[0]
+
+
+# In[203]:
+
+
+def read_files(i,dat=dat,fpp_out=fpp_out,name=name,vv=vv,zout=geo['zout']):
+    'function to feed the pool of workers to read all the files'
+    try:
+        f_in = '{name}_{vv}_DARE_{i:03d}_withaero.dat'.format(name=name,vv=vv,i=i)
+        o = Rl.read_libradtran(fpp_out+f_in,zout=geo['zout'])
+        f_in = '{name}_{vv}_star_{i:03d}_noaero.dat'.format(name=name,vv=vv,i=i)
+        on = Rl.read_libradtran(fpp_out+f_in,zout=geo['zout'])
+
+        dat['dn'][i,:] = o['diffuse_down']+o['direct_down']
+        dat['dn_noa'][i,:] = on['diffuse_down']+on['direct_down']
+        dat['up'][i,:] = o['diffuse_up']
+        dat['up_noa'][i,:] = on['diffuse_up']
+    except:
+        pass
+
+
+# In[ ]:
+
+
+outputs = p.map(read_files,range(len(ar['Start_UTC'][fla])))
+
+
+# For the original method (without multiprocessing)
+
+# In[133]:
+
+
+
+if isjupyter():
+    pbar = tqdm(total=n)
+    
+
+for i,u in enumerate(ar['Start_UTC'][fla]):
+    
+    dat['cod'][i] = cod[flb][i]
+    dat['ref'][i] = ref[flb][i]
+    dat['sza'][i] = sza[fla][i]
+
+    iae = np.argmin(abs(ar['time_ae'][fla][i]-ae['time']/3600.0))
+    # Only run for aerosol rertievals within 1 hour
+    if abs(ar['time_ae'][fla][i]-ae['time']/3600.0)[iae]<1.0: 
+
+        dat['ext'][i,:] = fx_ext(ar['AOD_polycoef_a0'][fla][i],ar['AOD_polycoef_a1'][fla][i],ar['AOD_polycoef_a2'][fla][i])[0]
+        dat['ssa'][i,:] = fx_aero(ae['SSA'][iae])[0]
         dat['asy'][i,:] = fx_aero(ae['g_total'][iae])[0]
         try:
             f_in = '{name}_{vv}_DARE_{i:03d}_withaero.dat'.format(name=name,vv=vv,i=i)
@@ -576,7 +808,7 @@ for i,u in enumerate(ar['Start_UTC'][fla]):
 dat['dare'] = (dat['dn']-dat['up']) - (dat['dn_noa']-dat['up_noa'])
 
 
-# In[ ]:
+# In[52]:
 
 
 dat['utc'] = ar['Start_UTC'][fla]
@@ -586,6 +818,14 @@ dat['doy'] = ar['doy'][fla]
 
 
 # ## Save the file
+
+# In[53]:
+
+
+dat1 = iterate_dict_unicode(dat)
+print 'saving file to: '+fp+'{name}_DARE_aero_prop_{vv}.mat'.format(name=name,vv=vv)
+hs.savemat(fp+'{name}_DARE_aero_prop_{vv}.mat'.format(name=name,vv=vv),dat1)
+
 
 # In[135]:
 
--- ORACLES_DARE_explore.py
+++ ORACLES_DARE_explore.py
@@ -39,7 +39,7 @@
 
 # # Prepare python environment
 
-# In[33]:
+# In[1]:
 
 
 import numpy as np
@@ -47,11 +47,10 @@ from path_utils import getpath
 import hdf5storage as hs
 import scipy.io as sio
 import matplotlib.pyplot as plt
-get_ipython().magic(u'matplotlib notebook')
 import os
 
 
-# In[85]:
+# In[2]:
 
 
 import Sp_parameters as Sp
@@ -60,7 +59,13 @@ import write_utils as wu
 import plotting_utils as pu
 
 
-# In[4]:
+# In[30]:
+
+
+get_ipython().magic(u'matplotlib notebook')
+
+
+# In[3]:
 
 
 name = 'ORACLES'
@@ -72,27 +77,39 @@ fp = getpath(name)
 
 # ## Load DARE calculations
 
-# In[7]:
+# In[4]:
 
 
-s = hs.loadmat(fp+'model/ORACLES_DARE_v1.mat')
+s = hs.loadmat(fp+'model/ORACLES_DARE_v2.mat')
 
 
-# In[8]:
+# In[5]:
 
 
 s.keys()
 
 
+# In[8]:
+
+
+ss = hs.loadmat(fp+'model/ORACLES_DARE_aero_prop_v1.mat')
+
+
+# In[9]:
+
+
+s['sza'] = ss['sza']
+
+
 # ## Load DARE parameters from SARE
 
-# In[113]:
+# In[11]:
 
 
 sa = sio.loadmat(fp+'ORACLES_2016_DARE_params_v2.mat')
 
 
-# In[114]:
+# In[12]:
 
 
 sa.keys()
@@ -100,25 +117,25 @@ sa.keys()
 
 # # Plot out data
 
-# In[9]:
+# In[13]:
 
 
 s['doy']
 
 
-# In[10]:
+# In[14]:
 
 
 s['doys'] = s['doy']+s['utc']/24.0
 
 
-# In[41]:
+# In[15]:
 
 
 ibad = (s['ref']==0.0) | (s['ref']>=25.0) | (s['cod']==0.0)
 
 
-# In[42]:
+# In[16]:
 
 
 s['cod'][ibad] = np.nan
@@ -130,7 +147,9 @@ s['up_noa'][ibad,:] = np.nan
 s['dare'][ibad,:] = np.nan
 
 
-# In[15]:
+# ## Plot the time trace
+
+# In[17]:
 
 
 plt.figure()
@@ -141,7 +160,7 @@ plt.grid()
 plt.title('ORACLES 2016 SSFR retrieved COD')
 
 
-# In[17]:
+# In[18]:
 
 
 plt.figure()
@@ -152,7 +171,9 @@ plt.grid()
 plt.title('ORACLES 2016 SSFR retrieved REF')
 
 
-# In[49]:
+# ## histogram of retrieved properties
+
+# In[19]:
 
 
 plt.figure()
@@ -161,7 +182,7 @@ plt.xlabel('COD')
 plt.title('ORACLES 2016 SSFR COD histogram')
 
 
-# In[51]:
+# In[20]:
 
 
 plt.figure()
@@ -170,13 +191,119 @@ plt.xlabel('REF [${{\\mu}}$m]')
 plt.title('ORACLES 2016 SSFR REF histogram')
 
 
-# In[108]:
+# In[71]:
+
+
+plt.figure()
+plt.hist(s['ref'],range=[0,50],bins=30,label='R$_{{eff}}$',alpha=0.6,normed=True,color='g')
+plt.hist(s['cod']*2,range=[0,50],bins=30,label='COD',alpha=0.6,normed=True,color='m')
+
+plt.axvline(np.nanmean(s['ref']),ls='-',color='g',label='mean')
+plt.axvline(np.nanmedian(s['ref']),ls='--',color='g',label='median')
+
+plt.axvline(np.nanmean(s['cod']),ls='-',color='m')
+plt.axvline(np.nanmedian(s['cod']),ls='--',color='m')
+plt.legend(frameon=False)
+
+plt.xlabel('R$_{{eff}}$ [$\mu$m], COD')
+plt.savefig(fp+'plot_DARE/ORACLES2016_COD_REF_hist.png',dpi=600,transparent=True)
+
+
+# In[21]:
+
+
+np.nanmean(s['cod']*2.0),np.nanmedian(s['cod']*2.0)
+
+
+# In[22]:
+
+
+np.nanmean(s['ref']),np.nanmedian(s['ref'])
+
+
+# In[23]:
+
+
+s['ssa'].shape
+
+
+# ## Aerosol products
+
+# In[92]:
+
+
+plt.figure()
+plt.plot(s['ssa'][:,2])
+
+
+# In[58]:
+
+
+plt.figure()
+plt.hist(s['ssa'][:,2],bins=30,range=[np.nanmin(s['ssa'][:,2]),np.nanmax(s['ssa'][:,2])],normed=True,alpha=0.6)
+plt.axvline(np.nanmean(s['ssa'][:,2]),ls='-',color='k',label='mean')
+plt.axvline(np.nanmedian(s['ssa'][:,2]),ls='--',color='k',label='median')
+plt.legend(frameon=False)
+plt.xlabel('SSA at 500 nm')
+plt.savefig(fp+'plot_DARE/ORACLES2016_SSA_500_hist.png',dpi=600,transparent=True)
+
+
+# In[75]:
+
+
+np.nanmean(s['ssa'][:,2]),np.nanmedian(s['ssa'][:,2])
+
+
+# In[61]:
+
+
+plt.figure()
+plt.hist(s['asy'][:,2],bins=30,range=[np.nanmin(s['asy'][:,2]),np.nanmax(s['asy'][:,2])],normed=True,alpha=0.6,color='orange')
+plt.axvline(np.nanmean(s['asy'][:,2]),ls='-',color='k',label='mean')
+plt.axvline(np.nanmedian(s['asy'][:,2]),ls='--',color='k',label='median')
+plt.legend(frameon=False)
+plt.xlabel('ASY at 500 nm')
+plt.savefig(fp+'plot_DARE/ORACLES2016_ASY_500_hist.png',dpi=600,transparent=True)
+
+
+# In[19]:
+
+
+s['wvl']
+
+
+# In[23]:
+
+
+plt.figure()
+plt.hist(s['asy'][:,5],range=[0.2,1],bins=30)
+
+
+# In[ ]:
+
+
+plt.figure()
+plt.hist(s['asy'][:,2],bins=30,range=[np.nanmin(s['asy'][:,2]),np.nanmax(s['asy'][:,2])],normed=True,alpha=0.6,color='orange')
+plt.axvline(np.nanmean(s['asy'][:,2]),ls='-',color='k',label='mean')
+plt.axvline(np.nanmedian(s['asy'][:,2]),ls='--',color='k',label='median')
+plt.legend(frameon=False)
+plt.xlabel('ASY at 500 nm')
+plt.savefig(fp+'plot_DARE/ORACLES2016_ASY_500_hist.png',dpi=600,transparent=True)
+
+
+# In[76]:
+
+
+np.nanmean(s['asy'][:,2]),np.nanmedian(s['asy'][:,2])
+
+
+# In[14]:
 
 
 s['ext'].shape
 
 
-# In[109]:
+# In[15]:
 
 
 s['wvl']
@@ -187,6 +314,59 @@ s['wvl']
 
 plt.figure()
 plt.hist(s['ext'][:,2],range=[np.nanmin(s['ext'][:,2]),np.nanmax(s['ext'][:,2])],bins=30)
+
+
+# ## DARE
+
+# In[32]:
+
+
+s.keys()
+
+
+# In[33]:
+
+
+import cmaps
+
+
+# In[34]:
+
+
+plt.figure()
+cmaps.cmaps()
+
+
+# In[35]:
+
+
+plt.figure()
+plt.hist2d(s['up'][:,2]/s['dn'][:,2],s['sza'],bins=40,range=[[0,1],[0,90]],cmap=plt.cm.jet)
+plt.ylabel('SZA [$^\\circ$]')
+plt.xlabel('TOA Albedo (BB)')
+plt.title('ORACLES 2016 Albedo from DARE calc')
+
+cb = plt.colorbar()
+cb.set_label('counts')
+pu.prelim()
+
+plt.savefig(fp+'plot/ORACLES_2016_albedo_from_calc_vs_SZA.png',dpi=600,transparent=True)
+
+
+# In[31]:
+
+
+plt.figure()
+plt.hist2d(s['dare'][:,2],s['sza'],bins=40,range=[[-70,150],[0,90]],cmap=plt.cm.Greens)
+plt.ylabel('SZA [$^\\circ$]')
+plt.xlabel('DARE [W/m$^2$]')
+plt.title('ORACLES 2016 DARE from P3 SSFR and 4STAR - DARE calc')
+
+cb = plt.colorbar()
+cb.set_label('counts')
+pu.prelim()
+
+plt.savefig(fp+'plot/ORACLES_2016_DARE_from_calc_vs_SZA.png',dpi=600,transparent=True)
 
 
 # In[53]:
@@ -205,7 +385,7 @@ plt.grid()
 plt.savefig(fp+'plot_DARE/ORACLES_2016_DARE.png',dpi=600,transparent=True)
 
 
-# In[65]:
+# In[93]:
 
 
 plt.figure()
@@ -220,6 +400,8 @@ plt.axvline(np.nanmedian(s['dare'][:,1]),ls='--',color='orange')
 plt.axvline(np.nanmean(s['dare'][:,0]),ls='-',color='g')
 plt.axvline(np.nanmedian(s['dare'][:,0]),ls='--',color='g')
 
+pu.prelim()
+
 plt.xlabel('DARE [W/m$^{{2}}$]')
 plt.legend(frameon=False)
 
@@ -227,24 +409,30 @@ plt.title('ORACLES 2016 DARE calculations')
 plt.savefig(fp+'plot_DARE/ORACLES_2016_DARE_hist.png',dpi=600,transparent=True)
 
 
-# In[75]:
+# In[34]:
 
 
 igood = np.isfinite(s['dare'][:,0])
 
 
-# In[87]:
+# In[35]:
 
 
 plt.figure()
 plt.hist(s['dare'][igood,2],bins=30,range=[-30,35])
 
 
+# In[36]:
+
+
+np.nanmean(s['dare'][igood,2]),np.nanmedian(s['dare'][igood,2]),np.nanstd(s['dare'][igood,2])
+
+
 # ## Make the boxes
 
 # The observations and model data are aggregated within horizontal domains of at least 2o by 2o indicated in Fig. 2. One of the three main regions encompasses the routine flight track, with individual grid boxes centered at (14oE, 24oS), (12oE, 22oS), (10oE, 20oS), (8oE, 18oS), (6oE, 16oS), (4oE, 14oS), (2oE, 12oS) and (0oE, 10oS). Another more coastal north-south track has the southernmost grid box centered on 22oS, spanning between 9oE and 11.75oE. Seven grid boxes are located every 2 degrees north of this, with the northernmost grid box centered on 8oS. A third, zonal track covers the larger domain of the ER2 measurements, with individual grid boxes spanning latitudinally between 10oS and 6oS and separated longitudinally at two degree intervals beginning at 3oW to the west and 13oE in the east. The box for St. Helena Island spans between 6.72 oW and 4.72 oW, between 16.933 oS and 14.933 oS.
 
-# In[66]:
+# In[37]:
 
 
 boxes_diag = []
@@ -252,7 +440,7 @@ boxes_ns = []
 boxes_ew = []
 
 
-# In[67]:
+# In[38]:
 
 
 boxes_diag_ct = [[14.0,-24.0], [12.0,-22.0],[10.0,-20.0],[8.0,-18.0],[6.0,-16.0],[4.0,-14.0],[2.0,-12.0],[0.0,-10.0]]
@@ -262,43 +450,43 @@ boxes_ew_ct = [[-3.0,-8.0],[-1.0,-8.0],[1.0,-8.0],[3.0,-8.0],[5.0,-8.0],[7.0,-8.
 
 # Corners are [x0,x1,y0,y1]
 
-# In[68]:
+# In[39]:
 
 
 boxes_ns = [[9.0,11.75,i[1]-1.0,i[1]+1.0] for i in boxes_ns_ct]
 
 
-# In[69]:
+# In[40]:
 
 
 boxes_ew = [[-10.0,-6.0,i[0]-1.0,i[0]+1.0] for i in boxes_ew_ct]
 
 
-# In[70]:
+# In[41]:
 
 
 boxes_diag = [[i[0]-1.0,i[0]+1,i[1]-1.0,i[1]+1.0] for i in boxes_diag_ct]
 
 
-# In[71]:
+# In[42]:
 
 
 boxes_diag
 
 
-# In[72]:
+# In[43]:
 
 
 boxes_ew
 
 
-# In[73]:
+# In[44]:
 
 
 boxes_ns
 
 
-# In[77]:
+# In[45]:
 
 
 bins_diag = []
@@ -307,13 +495,13 @@ for i,b in enumerate(boxes_diag):
     bins_diag.append(s['dare'][igood,2][ia])
 
 
-# In[100]:
+# In[46]:
 
 
 bins_diag[0] = bins_diag[1][0:5]
 
 
-# In[79]:
+# In[47]:
 
 
 bins_ns = []
@@ -322,13 +510,13 @@ for i,b in enumerate(boxes_ns):
     bins_ns.append(s['dare'][igood,2][ia])
 
 
-# In[105]:
+# In[48]:
 
 
 bins_ns[-1] = bins_ns[-2][0:5]
 
 
-# In[82]:
+# In[49]:
 
 
 bins_ew = []
@@ -337,22 +525,22 @@ for i,b in enumerate(boxes_ew):
     bins_ew.append(s['dare'][igood,2][ia])
 
 
-# In[83]:
+# In[50]:
 
 
 len(boxes_diag),len(bins_diag)
 
 
-# In[127]:
+# In[51]:
 
 
 [fig,ax] = plt.subplots(1,8,figsize=(13,3))
 
 for i,b in enumerate(boxes_diag_ct):
-    ax[i].hist(bins_diag[i],bins=30,edgecolor='None',alpha=0.7,color='g',range=(-30,100),
+    ax[i].hist(bins_diag[i],bins=30,edgecolor='None',alpha=0.7,color='g',range=(-20,35),
                zorder=10,normed=True,orientation='horizontal',label='Calculations')
-    #ax[i].hist(sa['bins_diag'][0,i][0],bins=30,edgecolor='None',alpha=0.3,color='m',range=(-30,100),
-    #           zorder=-1,normed=True,orientation='horizontal',label='Parameterization')
+    ax[i].hist(sa['bins_diag'][0,i][0],bins=30,edgecolor='None',alpha=0.3,color='m',range=(-30,100),
+               zorder=-1,normed=True,orientation='horizontal',label='Parameterization')
     ax[i].axhline(np.nanmean(bins_diag[i]),color='g',label='mean')
     ax[i].axhline(np.nanmedian(bins_diag[i]),color='g',linestyle='--',label='median')
     xmin, xmax = ax[i].get_xlim()
@@ -365,19 +553,25 @@ for i,b in enumerate(boxes_diag_ct):
     ax[i].axhline(0,ls=':',color='k',alpha=0.2)
     if i%2: pu.prelim(ax[i])
 ax[0].set_ylabel('DARE [W/m$^2$]')
-fig.suptitle('ORACLES 2016 Routine Diagonal (Lon,Lat) - 4STAR+SSFR DARE TOA calculations skyscans+cloud retr.')
+fig.suptitle('ORACLES 2016 Routine Diagonal (Lon,Lat) - TOA')
 fig.tight_layout()
-plt.savefig(fp+'plot_DARE/ORACLES_2016_DARE_TOA_calc_diag_boxes.png',dpi=600,transparent=True)
+#plt.savefig(fp+'plot_DARE/ORACLES_2016_DARE_TOA_calc_diag_boxes.png',dpi=600,transparent=True)
 
 
-# In[129]:
+# In[ ]:
+
+
+
+
+
+# In[53]:
 
 
 [fig,ax] = plt.subplots(1,8,figsize=(13,3))
 
 for i,b in enumerate(boxes_ns_ct):
-    ax[i].hist(bins_ns[i],bins=30,edgecolor='None',alpha=0.7,color='b',range=(-30,35),zorder=10,normed=True,orientation='horizontal')
-    #ax[i].hist(sa['bins_ns'][0,i][0],bins=30,edgecolor='None',alpha=0.2,color='orange',range=(-30,35),zorder=-1,normed=True,orientation='horizontal')
+    ax[i].hist(bins_ns[i],bins=30,edgecolor='None',alpha=0.7,color='b',range=(-50,100),zorder=10,normed=True,orientation='horizontal')
+    ax[i].hist(sa['bins_ns'][0,i][0],bins=30,edgecolor='None',alpha=0.2,color='orange',range=(-50,100),zorder=-1,normed=True,orientation='horizontal')
     ax[i].axhline(np.nanmean(bins_ns[i]),color='b',label='mean')
     ax[i].axhline(np.nanmedian(bins_ns[i]),color='b',linestyle='--',label='median')
     xmin, xmax = ax[i].get_xlim()
@@ -388,9 +582,9 @@ for i,b in enumerate(boxes_ns_ct):
     ax[i].axhline(0,ls=':',color='k',alpha=0.2)
     if i%2: pu.prelim(ax[i])
 ax[0].set_ylabel('DARE [W/m$^2$]')
-fig.suptitle('ORACLES 2016 North-South (Lon,Lat) - 4STAR+SSFR DARE TOA calculations skyscans+cloud retr.')
+fig.suptitle('ORACLES 2016 North-South (Lon,Lat) - TOA')
 fig.tight_layout()
-plt.savefig(fp+'plot_DARE/ORACLES_2016_DARE_TOA_calc_ns_boxes.png',dpi=600,transparent=True)
+#plt.savefig(fp+'plot_DARE/ORACLES_2016_DARE_TOA_calc_ns_boxes.png',dpi=600,transparent=True)
 
 
 # In[128]:
