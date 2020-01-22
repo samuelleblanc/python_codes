34425d28014a4c0feb97397126d5e3b5492b9734
Samuel LeBlanc <samuel.elie.leblanc@gmail.com>
2020-01-21 22:28:15 -0800

    New format for Kokhanovksy implemented

--- LeBlanc_2015_paper_file_restore.py
+++ LeBlanc_2015_paper_file_restore.py
@@ -328,6 +328,22 @@ plt.plot(wvl,sun)
 plt.plot(ku[:,0],ku[:,1],zorder=-1)
 
 
+# In[90]:
+
+
+i440 = np.argmin(abs(wvl-440.0))
+i1020 = np.argmin(abs(wvl-1020.0))
+i1640 = np.argmin(abs(wvl-1640.0))
+
+
+# In[126]:
+
+
+sun0440 = sun[i440]/1000.0
+sun1020 = sun[i1020]/1000.0
+sun1640 = sun[i1640]/1000.0
+
+
 # # Verify the results and write out
 
 # In[160]:
@@ -354,13 +370,13 @@ order = ['COD','COD_err_low','COD_err_up','REF','REF_err_low','REF_err_up','Phas
 
 # ## 15 parameters retrieval
 
-# In[53]:
+# In[100]:
 
 
 f_liq.keys()
 
 
-# In[54]:
+# In[101]:
 
 
 plt.figure()
@@ -732,7 +748,7 @@ for i,g in enumerate(twv):
     rtw.append(ftw)
 
 
-# In[148]:
+# In[72]:
 
 
 rtw[0]['REF']
@@ -1031,7 +1047,7 @@ wu.write_ict(hdict_sp_ice,dict_sp_ice,filepath=fp,
 # 
 #  
 
-# In[52]:
+# In[196]:
 
 
 hdict = {'PI':'Samuel LeBlanc',
@@ -1050,16 +1066,16 @@ hdict = {'PI':'Samuel LeBlanc',
      'stipulations':'',
      'rev_comments':'R0: Data from LeBlanc et al., 2015 publication, released to A. Kohkanovsky in January, 2020.'
     }
-order = ['SZA','T0440','T1020','T1640','COD','COD_err_low','COD_err_up','REF','REF_err_low','REF_err_up','Phase','Ki_square']
+order = ['DOY','SZA','T0440','T1020','T1640','COD','COD_err_low','COD_err_up','REF','REF_err_low','REF_err_up','Phase','LWP','Ki_square']
 
 
-# In[59]:
+# In[197]:
 
 
 f = [f_liq,f_mix,f_ice]
 
 
-# In[61]:
+# In[198]:
 
 
 def fo(gu,nn):
@@ -1075,7 +1091,7 @@ def fo(gu,nn):
     return gu
 
 
-# In[62]:
+# In[199]:
 
 
 rtr = []
@@ -1105,13 +1121,13 @@ for i,g in enumerate(f):
     rtr.append(ff)
 
 
-# In[63]:
+# In[200]:
 
 
 rtr[0].keys()
 
 
-# In[68]:
+# In[201]:
 
 
 lat = 40.01
@@ -1119,19 +1135,45 @@ lon = -105.25
 alt = 1660.0
 
 
-# In[69]:
+# In[202]:
 
 
 sp = [sp_liq,sp_mix,sp_ice]
 
 
-# In[70]:
+# In[203]:
 
 
 ut = [[15.0,16.0],[22.0,23.0],[17.5,19.5]]
 
 
-# In[71]:
+# In[204]:
+
+
+i440 = np.argmin(abs(sp[0]['zenlambda']-440.0))
+i1020 = np.argmin(abs(sp[0]['zenlambda']-1020.0))
+i1640 = np.argmin(abs(sp[0]['zenlambda']-1640.0))
+
+
+# In[205]:
+
+
+i440, i1020, i1640
+
+
+# In[206]:
+
+
+from math import pi
+
+
+# In[207]:
+
+
+from datetime import datetime
+
+
+# In[208]:
 
 
 rsp = []
@@ -1144,57 +1186,208 @@ for i,g in enumerate(sp):
     tp = (g['tmhrs']>=ut[i][0]) & (g['tmhrs']<=ut[i][1]) & (g['sat']==0) & (g['zspectra'][:,50]>0.0003)
     fsp['utc'] = g['tmhrs'][tp]
     fsp['sza'] = np.array(sza)[tp]
-    fsp['wvl'] = g['zenlambda']
-    fsp['rad'] = g['zspectra'][tp,:]
+    fsp['solfac'] = np.array(solfac)[tp]
+    fsp['s0440'] = g['zspectra'][tp,i440]
+    fsp['s1020'] = g['zspectra'][tp,i1020]
+    fsp['s1640'] = g['zspectra'][tp,i1640]
+    
+    fsp['t0440'] = fsp['s0440']*fsp['solfac']/np.cos(fsp['sza']*pi/180.0) / sun0440
+    fsp['t1020'] = fsp['s1020']*fsp['solfac']/np.cos(fsp['sza']*pi/180.0) / sun1020
+    fsp['t1640'] = fsp['s1640']*fsp['solfac']/np.cos(fsp['sza']*pi/180.0) / sun1640
+    
+    fsp['doy'] = np.array([ddt.timetuple().tm_yday+g['tmhrs'][j]/24.0 for j,ddt in enumerate(dtu)])
+    fsp['doy'] = fsp['doy'][tp]
     rsp.append(fsp)
+
+
+# In[209]:
+
+
+g['zspectra'][tp,i1640]
+
+
+# In[210]:
+
+
+plt.figure()
+plt.plot(rsp[0]['utc'],rsp[0]['t0440'],'.',label='440 nm')
+plt.plot(rsp[0]['utc'],rsp[0]['t1020'],'.',label='1020 nm')
+plt.plot(rsp[0]['utc'],rsp[0]['t1640'],'.',label='1640 nm')
+plt.legend(frameon=False)
+
+
+# In[211]:
+
+
+plt.figure()
+plt.plot(rsp[0]['utc'],rsp[0]['s0440'],'.',label='440 nm')
+plt.plot(rsp[0]['utc'],rsp[0]['s1020'],'.',label='1020 nm')
+plt.plot(rsp[0]['utc'],rsp[0]['s1640'],'.',label='1640 nm')
+plt.legend(frameon=False)
+
+
+# In[212]:
+
+
+from write_utils import nearest_neighbor
+
+
+# In[213]:
+
+
+for i,rr in enumerate(rtr):
+    rr['sza'] = nearest_neighbor(rsp[i]['utc'],rsp[i]['sza'],rr['tmhrs'],dist=2.0/3600.0)
+    rr['t0440'] = nearest_neighbor(rsp[i]['utc'],rsp[i]['t0440'],rr['tmhrs'],dist=2.0/3600.0)
+    rr['t1020'] = nearest_neighbor(rsp[i]['utc'],rsp[i]['t1020'],rr['tmhrs'],dist=2.0/3600.0)
+    rr['t1640'] = nearest_neighbor(rsp[i]['utc'],rsp[i]['t1640'],rr['tmhrs'],dist=2.0/3600.0)
+    rr['doy'] = nearest_neighbor(rsp[i]['utc'],rsp[i]['doy'],rr['tmhrs'],dist=2.0/3600.0)
+
+
+# In[214]:
+
+
+plt.figure()
+plt.plot(rtr[0]['tmhrs'],rtr[0]['t0440'])
 
 
 # ## Save liquid cloud in format
 
-# In[67]:
-
-
-s0440 = 
-
-
-# In[66]:
-
-
-T0440 = 
-
-
-# In[64]:
+# In[215]:
 
 
 dict_fliq =  {'Start_UTC':{'data':rtr[0]['tmhrs']*3600.0,'unit':'seconds from midnight UTC',
                            'long_description':'time keeping, based on UTC midnight'},
-      'COD':{'data':rtr[0]['tau_rtm'],'unit':'None','long_description':'Cloud Optical Depth of overlying cloud'},
-      'REF':{'data':rtr[0]['ref_rtm'],'unit':'micrometer',
+      'SZA':{'data':rtr[0]['sza'],'unit':'degrees','long_description':'Solar Zenith Angle of observations'},
+      'DOY':{'data':rtr[0]['doy'],'unit':'fractional day of year','format':'3.6f',
+                           'long_description':'number of days since the start of the year, representing portion of days as fractional.'},
+      'T0440':{'data':rtr[0]['t0440'],'unit':'None','long_description':'Atmospheric transmittance at 440 nm, using Kurudz [1992], top-of-atmosphere solar incident irradiance, adjusted for earth-sun distance and solar zenith angle.'},
+      'T1020':{'data':rtr[0]['t1020'],'unit':'None','long_description':'Atmospheric transmittance at 1020 nm, using Kurudz [1992], top-of-atmosphere solar incident irradiance, adjusted for earth-sun distance and solar zenith angle.'},
+      'T1640':{'data':rtr[0]['t1640'],'unit':'None','long_description':'Atmospheric transmittance at 1640 nm, using Kurudz [1992], top-of-atmosphere solar incident irradiance, adjusted for earth-sun distance and solar zenith angle.'},
+      'COD':{'data':rtr[0]['tau_rtm'],'unit':'None','long_description':'Cloud Optical Depth of overlying cloud','format':'2.1f'},
+      'REF':{'data':rtr[0]['ref_rtm'],'unit':'micrometer','format':'2.1f',
              'long_description':'Cloud drop effective radius for liquid clouds'},
-      'COD_err_low':{'data':rtr[0]['tau_err'][0,:],'unit':'None',
+      'LWP':{'data':5.0/9.0 * rtr[0]['tau_rtm']* rtr[0]['ref_rtm'],'unit':'g/meter^2','long_description':'Calculated Liquid Water Path of overlying cloud, (5/9*COD*REF) assuming linearly increasing effective radius with altitude (based on Wood and Hartmann, 2006, J. Clim., https://doi.org/10.1175/JCLI3702.1)'},
+      'COD_err_low':{'data':rtr[0]['tau_err'][0,:],'unit':'None','format':'2.1f',
                      'long_description':'Lower value of retrieval uncertainty of Cloud Optical Depth'},
-      'COD_err_up':{'data':rtr[0]['tau_err'][1,:],'unit':'None',
+      'COD_err_up':{'data':rtr[0]['tau_err'][1,:],'unit':'None','format':'2.1f',
                     'long_description':'Upper value of retrieval uncertainty of Cloud Optical Depth'},
-      'REF_err_low':{'data':rtr[0]['ref_err'][0,:],'unit':'micrometer',
+      'REF_err_low':{'data':rtr[0]['ref_err'][0,:],'unit':'micrometer','format':'2.1f',
                      'long_description':'Lower value of retrieval uncertainty of Cloud effective radius.'},
-      'REF_err_up':{'data':rtr[0]['ref_err'][1,:],'unit':'micrometer',
+      'REF_err_up':{'data':rtr[0]['ref_err'][1,:],'unit':'micrometer','format':'2.1f',
                     'long_description':'Upper value of retrieval uncertainty of Cloud effective radius.'},
-      'Phase':{'data':rtr[0]['wp_rtm'],'unit':'None',
+      'Phase':{'data':rtr[0]['wp_rtm'],'unit':'None','format':'1.0f',
                'long_description':'Thermodynamic phase, 0 for liquid cloud, 1 for ice cloud'},
       'Ki_square':{'data':rtr[0]['ki_rtm'],'unit':'None',
                    'long_description':'Ki square fit parameter. It is the remainder of the ki square fit, values higher than 0.69 are considered to be failed retrievals.'}}
 
 
-# In[65]:
+# In[216]:
 
 
 hdict_fliq = hdict
 hdict_fliq['special_comments'] = 'Liquid cloud case from LeBlanc et al., 2015'
 
 
-# In[ ]:
+# In[217]:
 
 
 wu.write_ict(hdict_fliq,dict_fliq,filepath=fp,
-              data_id='SSFR_15params_CLD',loc_id='Boulder',date=days[0],rev='R0',order=order,delim=' ')  
+              data_id='SSFR_Trans15params_CLD',loc_id='Boulder',date=days[0],rev='R0',order=order,delim=' ')  
+
+
+# ## Save mix-phase cloud in format
+
+# In[218]:
+
+
+dict_fmix =  {'Start_UTC':{'data':rtr[1]['tmhrs']*3600.0,'unit':'seconds from midnight UTC',
+                           'long_description':'time keeping, based on UTC midnight'},
+      'SZA':{'data':rtr[1]['sza'],'unit':'degrees','long_description':'Solar Zenith Angle of observations'},
+      'DOY':{'data':rtr[1]['doy'],'unit':'fractional day of year','format':'3.6f',
+             'long_description':'number of days since the start of the year, representing portion of days as fractional.'},
+      'T0440':{'data':rtr[1]['t0440'],'unit':'None','long_description':'Atmospheric transmittance at 440 nm, using Kurudz [1992], top-of-atmosphere solar incident irradiance, adjusted for earth-sun distance and solar zenith angle.'},
+      'T1020':{'data':rtr[1]['t1020'],'unit':'None','long_description':'Atmospheric transmittance at 1020 nm, using Kurudz [1992], top-of-atmosphere solar incident irradiance, adjusted for earth-sun distance and solar zenith angle.'},
+      'T1640':{'data':rtr[1]['t1640'],'unit':'None','long_description':'Atmospheric transmittance at 1640 nm, using Kurudz [1992], top-of-atmosphere solar incident irradiance, adjusted for earth-sun distance and solar zenith angle.'},
+      'COD':{'data':rtr[1]['tau_rtm'],'unit':'None','long_description':'Cloud Optical Depth of overlying cloud','format':'2.1f'},
+      'REF':{'data':rtr[1]['ref_rtm'],'unit':'micrometer','format':'2.1f',
+             'long_description':'Cloud drop effective radius for liquid clouds'},
+      'LWP':{'data':5.0/9.0 * rtr[1]['tau_rtm']* rtr[1]['ref_rtm'],'unit':'g/meter^2','long_description':'Calculated Liquid Water Path of overlying cloud, (5/9*COD*REF) assuming linearly increasing effective radius with altitude (based on Wood and Hartmann, 2006, J. Clim., https://doi.org/10.1175/JCLI3702.1)'},
+      'COD_err_low':{'data':rtr[1]['tau_err'][0,:],'unit':'None','format':'2.1f',
+                     'long_description':'Lower value of retrieval uncertainty of Cloud Optical Depth'},
+      'COD_err_up':{'data':rtr[1]['tau_err'][1,:],'unit':'None','format':'2.1f',
+                    'long_description':'Upper value of retrieval uncertainty of Cloud Optical Depth'},
+      'REF_err_low':{'data':rtr[1]['ref_err'][0,:],'unit':'micrometer','format':'2.1f',
+                     'long_description':'Lower value of retrieval uncertainty of Cloud effective radius.'},
+      'REF_err_up':{'data':rtr[1]['ref_err'][1,:],'unit':'micrometer','format':'2.1f',
+                    'long_description':'Upper value of retrieval uncertainty of Cloud effective radius.'},
+      'Phase':{'data':rtr[1]['wp_rtm'],'unit':'None','format':'1.0f',
+               'long_description':'Thermodynamic phase, 0 for liquid cloud, 1 for ice cloud'},
+      'Ki_square':{'data':rtr[1]['ki_rtm'],'unit':'None',
+                   'long_description':'Ki square fit parameter. It is the remainder of the ki square fit, values higher than 0.69 are considered to be failed retrievals.'}}
+
+
+# In[219]:
+
+
+hdict_mix = hdict
+hdict_mix['special_comments'] = 'Mixed-phase cloud case from LeBlanc et al., 2015'
+
+
+# In[220]:
+
+
+wu.write_ict(hdict_mix,dict_fmix,filepath=fp,
+              data_id='SSFR_Trans15params_CLD',loc_id='Boulder',date=days[1],rev='R0',order=order,delim=' ') 
+
+
+# ## Save ice cloud in format
+
+# In[221]:
+
+
+dict_fice =  {'Start_UTC':{'data':rtr[2]['tmhrs']*3600.0,'unit':'seconds from midnight UTC',
+                           'long_description':'time keeping, based on UTC midnight'},
+      'SZA':{'data':rtr[2]['sza'],'unit':'degrees','long_description':'Solar Zenith Angle of observations'},
+      'DOY':{'data':rtr[2]['doy'],'unit':'fractional day of year','format':'3.6f',
+             'long_description':'number of days since the start of the year, representing portion of days as fractional.'},
+      'T0440':{'data':rtr[2]['t0440'],'unit':'None','long_description':'Atmospheric transmittance at 440 nm, using Kurudz [1992], top-of-atmosphere solar incident irradiance, adjusted for earth-sun distance and solar zenith angle.'},
+      'T1020':{'data':rtr[2]['t1020'],'unit':'None','long_description':'Atmospheric transmittance at 1020 nm, using Kurudz [1992], top-of-atmosphere solar incident irradiance, adjusted for earth-sun distance and solar zenith angle.'},
+      'T1640':{'data':rtr[2]['t1640'],'unit':'None','long_description':'Atmospheric transmittance at 1640 nm, using Kurudz [1992], top-of-atmosphere solar incident irradiance, adjusted for earth-sun distance and solar zenith angle.'},
+      'COD':{'data':rtr[2]['tau_rtm'],'unit':'None','long_description':'Cloud Optical Depth of overlying cloud','format':'2.1f'},
+      'REF':{'data':rtr[2]['ref_rtm'],'unit':'micrometer','format':'2.1f',
+             'long_description':'Cloud drop effective radius for liquid clouds'},
+      'LWP':{'data':5.0/9.0 * rtr[2]['tau_rtm']* rtr[2]['ref_rtm'],'unit':'g/meter^2',
+             'long_description':'Calculated Liquid Water Path of overlying cloud, (5/9*COD*REF) assuming linearly increasing effective radius with altitude (based on Wood and Hartmann, 2006, J. Clim., https://doi.org/10.1175/JCLI3702.1)'},
+      'COD_err_low':{'data':rtr[2]['tau_err'][0,:],'unit':'None','format':'2.1f',
+                     'long_description':'Lower value of retrieval uncertainty of Cloud Optical Depth'},
+      'COD_err_up':{'data':rtr[2]['tau_err'][1,:],'unit':'None','format':'2.1f',
+                    'long_description':'Upper value of retrieval uncertainty of Cloud Optical Depth'},
+      'REF_err_low':{'data':rtr[2]['ref_err'][0,:],'unit':'micrometer','format':'2.1f',
+                     'long_description':'Lower value of retrieval uncertainty of Cloud effective radius.'},
+      'REF_err_up':{'data':rtr[2]['ref_err'][1,:],'unit':'micrometer','format':'2.1f',
+                    'long_description':'Upper value of retrieval uncertainty of Cloud effective radius.'},
+      'Phase':{'data':rtr[2]['wp_rtm'],'unit':'None','format':'1.0f',
+               'long_description':'Thermodynamic phase, 0 for liquid cloud, 1 for ice cloud'},
+      'Ki_square':{'data':rtr[2]['ki_rtm'],'unit':'None',
+                   'long_description':'Ki square fit parameter. It is the remainder of the ki square fit, values higher than 0.69 are considered to be failed retrievals.'}}
+
+
+# In[222]:
+
+
+hdict_ice = hdict
+hdict_ice['special_comments'] = 'Ice cloud case from LeBlanc et al., 2015'
+
+
+# In[224]:
+
+
+wu.write_ict(hdict_ice,dict_fice,filepath=fp,
+              data_id='SSFR_Trans15params_CLD',loc_id='Boulder',date=days[2],rev='R0',order=order,delim=' ') 
+
+
+# In[ ]:
+
+
+
 
