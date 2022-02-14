dec8b26e21b04887a26214ddb70e0bfc62c5b656
Samuel LeBlanc <samuel.elie.leblanc@gmail.com>
2022-02-14 15:16:07 -0800

    OXA vs height comparisons for all ORACLES

--- ORACLES_aerosol_layer_height_vs_Oxa.py
+++ ORACLES_aerosol_layer_height_vs_Oxa.py
@@ -42,7 +42,7 @@
 
 # # Prepare python environment
 
-# In[41]:
+# In[2]:
 
 
 import numpy as np
@@ -58,7 +58,7 @@ import os
 from scipy import interpolate
 
 
-# In[4]:
+# In[3]:
 
 
 name = 'ORACLES'
@@ -66,7 +66,7 @@ vv = 'v1'
 fp = getpath(name)
 
 
-# In[8]:
+# In[4]:
 
 
 fpo = '/data/sunsat/ORACLES_2016/data_processed/starsuns/R3/'
@@ -76,44 +76,58 @@ fpo = '/data/sunsat/ORACLES_2016/data_processed/starsuns/R3/'
 
 # ## Load gap files
 
-# In[112]:
+# In[5]:
 
 
 gap6 = np.load(fp+'ORACLES2016_gap_{}.npy'.format(vv),allow_pickle=True,fix_imports=True,encoding='latin1')
 gap6 = gap6.item()
 
 
-# In[6]:
+# In[13]:
 
 
 gap7 = np.load(fp+'ORACLES2017_gap_{}.npy'.format(vv),allow_pickle=True,fix_imports=True,encoding='latin1')
 gap7 = gap7.item()
 
 
-# In[7]:
+# In[14]:
 
 
 gap8 = np.load(fp+'ORACLES2018_gap_{}.npy'.format(vv),allow_pickle=True,fix_imports=True,encoding='latin1')
 gap8 = gap8.item()
 
 
-# In[9]:
+# In[15]:
 
 
 gap6.keys()
 
 
-# In[24]:
+# In[9]:
 
 
 days6 = [d.strftime('%Y%m%d') for d in gap6['aero_base_day']]
 day6 = np.unique(days6)
 
 
-# In[25]:
+# In[10]:
 
 
 day6
+
+
+# In[16]:
+
+
+days7 = [d.strftime('%Y%m%d') for d in gap7['aero_base_day']]
+day7 = np.unique(days7)
+
+
+# In[19]:
+
+
+days8 = [d.strftime('%Y%m%d') for d in gap8['aero_base_day']]
+day8 = np.unique(days8)
 
 
 # ## Load starsun files
@@ -136,22 +150,74 @@ for d in day6:
 len(s6)
 
 
-# In[30]:
+# In[29]:
 
 
 s6[0].keys()
 
 
-# In[31]:
+# In[30]:
 
 
 for ss in s6:
     ss['utc'] = lu.mat2py_time(ss['t'])
 
 
+# ### Load 2017
+
+# In[20]:
+
+
+fpo7 = '/data/sunsat/ORACLES_2017/data_processed/starsuns/R0/'
+
+
+# In[22]:
+
+
+s7 = []
+for d in day7:
+    ss = hs.loadmat(fpo7+'4STAR_{}starsun.mat'.format(d),variable_names=['tau_aero','w','t','Pst'])
+    ss['filename'] = fpo7+'4STAR_{}starsun.mat'.format(d)
+    print(ss['filename'],'/',len(day7))
+    s7.append(ss)
+
+
+# In[31]:
+
+
+for ss in s7:
+    ss['utc'] = lu.mat2py_time(ss['t'])
+
+
+# ### Load 2018
+
+# In[23]:
+
+
+fpo8 = '/data/sunsat/ORACLES_2018/data_processed/starsuns/R0/'
+
+
+# In[24]:
+
+
+s8 = []
+for d in day8:
+    ss = hs.loadmat(fpo8+'4STAR_{}starsun.mat'.format(d),variable_names=['tau_aero','w','t','Pst'])
+    ss['filename'] = fpo8+'4STAR_{}starsun.mat'.format(d)
+    print(ss['filename'])
+    s8.append(ss)
+
+
+# In[32]:
+
+
+for ss in s8:
+    ss['utc'] = lu.mat2py_time(ss['t'])
+
+
 # ## Calculate the Ox-a depth
 
-# In[39]:
+# In[33]:
 
 
 def oxa_depth(w,spec):
@@ -166,7 +232,9 @@ def oxa_depth(w,spec):
     return np.array(oxa_delta)
 
 
-# In[53]:
+# ### 2016 Ox-A
+
+# In[34]:
 
 
 for j,ss in enumerate(s6):
@@ -180,33 +248,67 @@ for j,ss in enumerate(s6):
         pass
 
 
+# ### 2017 Ox-A
+
+# In[35]:
+
+
+for j,ss in enumerate(s7):
+    print('Doing file {}, which is {}/{}'.format(day7[j],j,len(s7)))
+    if 'oxa_delta' in ss.keys(): 
+        print('Yas queen already in there')
+        continue
+    try:
+        ss['oxa_delta'] = oxa_depth(ss['w'],ss['tau_aero'])
+    except:
+        pass
+
+
+# ### 2018 Ox-A
+
+# In[36]:
+
+
+for j,ss in enumerate(s8):
+    print('Doing file {}, which is {}/{}'.format(day8[j],j,len(s8)))
+    if 'oxa_delta' in ss.keys(): 
+        print('Yas queen already in there')
+        continue
+    try:
+        ss['oxa_delta'] = oxa_depth(ss['w'],ss['tau_aero'])
+    except:
+        pass
+
+
 # ## Get the oxa depth for the gap times
 
-# In[75]:
+# In[37]:
 
 
 dd = gap6['aero_base_day'][1]
 
 
-# In[80]:
+# In[38]:
 
 
 utc_fx = lambda x: x.hour+(x.minute+(x.second/60.0))/60.0
 
 
-# In[95]:
+# In[39]:
 
 
 s6[iu].keys()
 
 
-# In[111]:
+# In[40]:
 
 
 len(gap6['aero_base_alt'])
 
 
-# In[124]:
+# ### 2016
+
+# In[45]:
 
 
 n = len(gap6['aero_base_day'])
@@ -224,10 +326,50 @@ for i,dd in enumerate(gap6['aero_base_day']):
     aod[i] = s6[iu]['tau_aero'][it2,407]
 
 
-# In[123]:
+# In[42]:
 
 
 s6[iu]['w'][0,407]
+
+
+# ### 2017
+
+# In[44]:
+
+
+n = len(gap7['aero_base_day'])
+oxa_aero_base7,oxa_meas7,press_meas7,aod7 = np.zeros((n))+np.nan,np.zeros((n))+np.nan,np.zeros((n))+np.nan,np.zeros((n))+np.nan 
+for i,dd in enumerate(gap7['aero_base_day']):
+    iu = np.where(day7==days7[i])[0][0]
+    it = np.argmin(abs(dd-s7[iu]['utc']))
+    try:
+        oxa_aero_base7[i] = s7[iu]['oxa_delta'][it]
+    except KeyError:
+        continue
+    it2 = np.argmin(abs(gap7['meas_low_day'][i]-s7[iu]['utc']))
+    oxa_meas7[i] = s7[iu]['oxa_delta'][it2]
+    press_meas7[i] = s7[iu]['Pst'][it2]
+    aod7[i] = s7[iu]['tau_aero'][it2,407]
+
+
+# ### 2018
+
+# In[46]:
+
+
+n = len(gap8['aero_base_day'])
+oxa_aero_base8,oxa_meas8,press_meas8,aod8 = np.zeros((n))+np.nan,np.zeros((n))+np.nan,np.zeros((n))+np.nan,np.zeros((n))+np.nan 
+for i,dd in enumerate(gap8['aero_base_day']):
+    iu = np.where(day8==days8[i])[0][0]
+    it = np.argmin(abs(dd-s8[iu]['utc']))
+    try:
+        oxa_aero_base8[i] = s8[iu]['oxa_delta'][it]
+    except KeyError:
+        continue
+    it2 = np.argmin(abs(gap8['meas_low_day'][i]-s8[iu]['utc']))
+    oxa_meas8[i] = s8[iu]['oxa_delta'][it2]
+    press_meas8[i] = s8[iu]['Pst'][it2]
+    aod8[i] = s8[iu]['tau_aero'][it2,407]
 
 
 # # Plot out data
@@ -239,36 +381,36 @@ plt.figure()
 plt.plot(s6[0]['tau_aero'][:,500],s6[0]['oxa_delta'],'.')
 
 
-# In[117]:
+# In[47]:
 
 
 oxa_meas[oxa_meas==0.0] = np.nan
 
 
-# In[137]:
+# In[48]:
 
 
 import plotting_utils as pu
 
 
-# In[174]:
+# In[90]:
 
 
 plt.figure()
-i_low = (press_meas>880.0) & (press_meas<950.0) & (aod>0.0) & (aod<0.2)
-plt.scatter(oxa_meas[i_low],gap6['aero_base_alt'][i_low],
-            label='AOD>0.0 to AOD<0.2, R={:1.2f}'.format(np.corrcoef(gap6['aero_base_alt'][i_low],oxa_meas[i_low])[1,0]))
-pu.plot_lin(oxa_meas[i_low],gap6['aero_base_alt'][i_low],color='tab:blue')
+i_lowa = (press_meas>880.0) & (press_meas<950.0) & (aod>0.0) & (aod<0.2)
+plt.scatter(oxa_meas[i_lowa],gap6['aero_base_alt'][i_lowa],
+            label='AOD>0.0 to AOD<0.2, R={:1.2f}'.format(np.corrcoef(gap6['aero_base_alt'][i_lowa],oxa_meas[i_lowa])[1,0]))
+pu.plot_lin(oxa_meas[i_lowa],gap6['aero_base_alt'][i_lowa],color='tab:blue',lblfmt='2.4f')
 
-i_low = (press_meas>880.0) & (press_meas<950.0)& (aod>0.1) & (aod<0.3)
-plt.scatter(oxa_meas[i_low],gap6['aero_base_alt'][i_low],
-            label='AOD>0.1 to AOD<0.3, R={:1.2f}'.format(np.corrcoef(gap6['aero_base_alt'][i_low],oxa_meas[i_low])[1,0]))
-pu.plot_lin(oxa_meas[i_low],gap6['aero_base_alt'][i_low],color='tab:orange')
+i_lowb = (press_meas>880.0) & (press_meas<950.0)& (aod>0.1) & (aod<0.3)
+plt.scatter(oxa_meas[i_lowb],gap6['aero_base_alt'][i_lowb],
+            label='AOD>0.1 to AOD<0.3, R={:1.2f}'.format(np.corrcoef(gap6['aero_base_alt'][i_lowb],oxa_meas[i_lowb])[1,0]))
+pu.plot_lin(oxa_meas[i_lowb],gap6['aero_base_alt'][i_lowb],color='tab:orange',lblfmt='2.4f')
 
-i_low = (press_meas>880.0) & (press_meas<950.0) & (aod>0.4)
-plt.scatter(oxa_meas[i_low],gap6['aero_base_alt'][i_low],
-            label='AOD>0.4, R={:1.2f}'.format(np.corrcoef(gap6['aero_base_alt'][i_low],oxa_meas[i_low])[1,0]))
-pu.plot_lin(oxa_meas[i_low],gap6['aero_base_alt'][i_low],color='tab:green')
+i_lowc = (press_meas>880.0) & (press_meas<950.0) & (aod>0.4)
+plt.scatter(oxa_meas[i_lowc],gap6['aero_base_alt'][i_lowc],
+            label='AOD>0.4, R={:1.2f}'.format(np.corrcoef(gap6['aero_base_alt'][i_lowc],oxa_meas[i_lowc])[1,0]))
+pu.plot_lin(oxa_meas[i_lowc],gap6['aero_base_alt'][i_lowc],color='tab:green',lblfmt='2.4f')
 
 #i_low = (press_meas>880.0) & (press_meas<950.0) & (aod>0.6)
 #plt.scatter(oxa_meas[i_low],gap6['aero_base_alt'][i_low],label='AOD>0.6')
@@ -279,6 +421,175 @@ plt.xlabel('Depth of Oxygen-A [sum of optical depth deviation]')
 plt.ylabel('Aerosol layer base height [m]')
 plt.title('ORACLES 2016\nAerosol height, Ox-A, and AOD from 4STAR\n[measured between 880 mb to 950 mb]')
 plt.savefig(fp+'ORACLES2016_OXa_vs_aero_alt_{}.png'.format(vv),dpi=600,transparent=True)
+
+
+# In[62]:
+
+
+press_meas7
+
+
+# In[89]:
+
+
+plt.figure()
+i_lowa7 = (press_meas7>850.0) & (press_meas7<920.0) & (aod7>0.0) & (aod7<0.25)
+plt.scatter(oxa_meas7[i_lowa7],gap7['aero_base_alt'][i_lowa7],
+        label='AOD>0.0 to AOD<0.2, R={:1.2f}'.format(np.corrcoef(gap7['aero_base_alt'][i_lowa7],oxa_meas7[i_lowa7])[1,0]))
+pu.plot_lin(oxa_meas7[i_lowa7],gap7['aero_base_alt'][i_lowa7],color='tab:blue',lblfmt='2.4f')
+
+i_lowb7 = (press_meas7>850.0) & (press_meas7<920.0)& (aod7>0.1) & (aod7<0.3)
+plt.scatter(oxa_meas7[i_lowb7],gap7['aero_base_alt'][i_lowb7],
+        label='AOD>0.1 to AOD<0.3, R={:1.2f}'.format(np.corrcoef(gap7['aero_base_alt'][i_lowb7],oxa_meas7[i_lowb7])[1,0]))
+pu.plot_lin(oxa_meas7[i_lowb7],gap7['aero_base_alt'][i_lowb7],color='tab:orange',lblfmt='2.4f')
+
+i_lowc7 = (press_meas7>850.0) & (press_meas7<920.0) & (aod7>0.3)
+plt.scatter(oxa_meas7[i_lowc7],gap7['aero_base_alt'][i_lowc7],
+        label='AOD>0.3, R={:1.2f}'.format(np.corrcoef(gap7['aero_base_alt'][i_lowc7],oxa_meas7[i_lowc7])[1,0]))
+pu.plot_lin(oxa_meas7[i_lowc7],gap7['aero_base_alt'][i_lowc7],color='tab:green',lblfmt='2.4f')
+
+#i_low = (press_meas>880.0) & (press_meas<950.0) & (aod>0.6)
+#plt.scatter(oxa_meas[i_low],gap6['aero_base_alt'][i_low],label='AOD>0.6')
+#pu.plot_lin(oxa_meas[i_low],gap6['aero_base_alt'][i_low],color='tab:red')
+#plt.colorbar(label='Pressure [mb]')
+plt.legend()
+plt.xlabel('Depth of Oxygen-A [sum of optical depth deviation]')
+plt.ylabel('Aerosol layer base height [m]')
+plt.title('ORACLES 2017\nAerosol height, Ox-A, and AOD from 4STAR\n[measured between 850 mb to 920 mb]')
+plt.savefig(fp+'ORACLES2017_OXa_vs_aero_alt_850mb_{}.png'.format(vv),dpi=600,transparent=True)
+
+
+# In[88]:
+
+
+plt.figure()
+i_lowa7 = (press_meas7>880.0) & (press_meas7<950.0) & (aod7>0.0) & (aod7<0.25)
+plt.scatter(oxa_meas7[i_lowa7],gap7['aero_base_alt'][i_lowa7],
+        label='AOD>0.0 to AOD<0.2, R={:1.2f}'.format(np.corrcoef(gap7['aero_base_alt'][i_lowa7],oxa_meas7[i_lowa7])[1,0]))
+pu.plot_lin(oxa_meas7[i_lowa7],gap7['aero_base_alt'][i_lowa7],color='tab:blue',lblfmt='2.4f')
+
+i_lowb7 = (press_meas7>880.0) & (press_meas7<950.0)& (aod7>0.1) & (aod7<0.3)
+plt.scatter(oxa_meas7[i_lowb7],gap7['aero_base_alt'][i_lowb7],
+        label='AOD>0.1 to AOD<0.3, R={:1.2f}'.format(np.corrcoef(gap7['aero_base_alt'][i_lowb7],oxa_meas7[i_lowb7])[1,0]))
+pu.plot_lin(oxa_meas7[i_lowb7],gap7['aero_base_alt'][i_lowb7],color='tab:orange',lblfmt='2.4f')
+
+i_lowc7 = (press_meas7>880.0) & (press_meas7<950.0) & (aod7>0.3)
+plt.scatter(oxa_meas7[i_lowc7],gap7['aero_base_alt'][i_lowc7],
+        label='AOD>0.3, R={:1.2f}'.format(np.corrcoef(gap7['aero_base_alt'][i_lowc7],oxa_meas7[i_lowc7])[1,0]))
+pu.plot_lin(oxa_meas7[i_lowc7],gap7['aero_base_alt'][i_lowc7],color='tab:green',lblfmt='2.4f')
+
+#i_low = (press_meas>880.0) & (press_meas<950.0) & (aod>0.6)
+#plt.scatter(oxa_meas[i_low],gap6['aero_base_alt'][i_low],label='AOD>0.6')
+#pu.plot_lin(oxa_meas[i_low],gap6['aero_base_alt'][i_low],color='tab:red')
+#plt.colorbar(label='Pressure [mb]')
+plt.legend()
+plt.xlabel('Depth of Oxygen-A [sum of optical depth deviation]')
+plt.ylabel('Aerosol layer base height [m]')
+plt.title('ORACLES 2017\nAerosol height, Ox-A, and AOD from 4STAR\n[measured between 880 mb to 950 mb]')
+plt.savefig(fp+'ORACLES2017_OXa_vs_aero_alt_{}.png'.format(vv),dpi=600,transparent=True)
+
+
+# In[87]:
+
+
+plt.figure()
+i_lowa8 = (press_meas8>850.0) & (press_meas8<930.0) & (aod8>0.0) & (aod8<0.25)
+plt.scatter(oxa_meas8[i_lowa8],gap8['aero_base_alt'][i_lowa8],
+            label='AOD>0.0 to AOD<0.2, R={:1.2f}'.format(np.corrcoef(gap8['aero_base_alt'][i_lowa8],oxa_meas8[i_lowa8])[1,0]))
+pu.plot_lin(oxa_meas8[i_lowa8],gap8['aero_base_alt'][i_lowa8],color='tab:blue',lblfmt='2.4f')
+
+i_lowb8 = (press_meas8>850.0) & (press_meas8<930.0)& (aod8>0.1) & (aod8<0.3)
+plt.scatter(oxa_meas8[i_lowb8],gap8['aero_base_alt'][i_lowb8],
+            label='AOD>0.1 to AOD<0.3, R={:1.2f}'.format(np.corrcoef(gap8['aero_base_alt'][i_lowb8],oxa_meas8[i_lowb8])[1,0]))
+pu.plot_lin(oxa_meas8[i_lowb8],gap8['aero_base_alt'][i_lowb8],color='tab:orange',lblfmt='2.4f')
+
+i_lowc8 = (press_meas8>850.0) & (press_meas8<930.0) & (aod8>0.3)
+plt.scatter(oxa_meas8[i_lowc8],gap8['aero_base_alt'][i_lowc8],
+            label='AOD>0.3, R={:1.2f}'.format(np.corrcoef(gap8['aero_base_alt'][i_lowc8],oxa_meas8[i_lowc8])[1,0]))
+pu.plot_lin(oxa_meas8[i_lowc8],gap8['aero_base_alt'][i_lowc8],color='tab:green',lblfmt='2.4f')
+
+#i_low = (press_meas>880.0) & (press_meas<950.0) & (aod>0.6)
+#plt.scatter(oxa_meas[i_low],gap6['aero_base_alt'][i_low],label='AOD>0.6')
+#pu.plot_lin(oxa_meas[i_low],gap6['aero_base_alt'][i_low],color='tab:red')
+#plt.colorbar(label='Pressure [mb]')
+plt.legend()
+plt.xlabel('Depth of Oxygen-A [sum of optical depth deviation]')
+plt.ylabel('Aerosol layer base height [m]')
+plt.title('ORACLES 2018\nAerosol height, Ox-A, and AOD from 4STAR\n[measured between 850 mb to 930 mb]')
+plt.savefig(fp+'ORACLES2018_OXa_vs_aero_alt_{}.png'.format(vv),dpi=600,transparent=True)
+
+
+# In[69]:
+
+
+gap8['aero_base_alt']
+
+
+# ## Combined plots
+
+# In[100]:
+
+
+np.append(np.append(oxa_meas[i_lowa],oxa_meas7[i_lowa7]),oxa_meas8[i_lowa8])
+
+
+# In[110]:
+
+
+plt.figure()
+
+plt.scatter(oxa_meas[i_lowa],gap6['aero_base_alt'][i_lowa],marker='.',color='tab:blue')
+#            label='AOD>0.0 to AOD<0.2, R={:1.2f}'.format(np.corrcoef(gap6['aero_base_alt'][i_lowa],oxa_meas[i_lowa])[1,0]))
+plt.scatter(oxa_meas7[i_lowa7],gap7['aero_base_alt'][i_lowa7],marker='+',color='tab:blue')
+#        label='AOD>0.0 to AOD<0.2, R={:1.2f}'.format(np.corrcoef(gap7['aero_base_alt'][i_lowa7],oxa_meas7[i_lowa7])[1,0]))
+#pu.plot_lin(oxa_meas7[i_lowa7],gap7['aero_base_alt'][i_lowa7],color='tab:blue',lblfmt='2.4f')
+plt.scatter(oxa_meas8[i_lowa8],gap8['aero_base_alt'][i_lowa8],marker='^',color='tab:blue')
+#            label='AOD>0.0 to AOD<0.2, R={:1.2f}'.format(np.corrcoef(gap8['aero_base_alt'][i_lowa8],oxa_meas8[i_lowa8])[1,0]))
+#pu.plot_lin(oxa_meas8[i_lowa8],gap8['aero_base_alt'][i_lowa8],color='tab:blue',lblfmt='2.4f')
+
+oxa_a = np.append(np.append(oxa_meas[i_lowa],oxa_meas7[i_lowa7]),oxa_meas8[i_lowa8])
+bas_a = np.append(np.append(gap6['aero_base_alt'][i_lowa],gap7['aero_base_alt'][i_lowa7]),gap8['aero_base_alt'][i_lowa8])
+plt.scatter(oxa_meas[i_lowa],gap6['aero_base_alt'][i_lowa],marker='.',color='tab:blue',
+            label='AOD>0.0 to AOD<0.2, R={:1.2f}'.format(np.corrcoef(bas_a,oxa_a)[1,0]))
+pu.plot_lin(oxa_a,bas_a,color='tab:blue',lblfmt='2.4f')
+
+plt.scatter(oxa_meas[i_lowb],gap6['aero_base_alt'][i_lowb],marker='.',color='tab:orange')
+#            label='AOD>0.1 to AOD<0.3, R={:1.2f}'.format(np.corrcoef(gap6['aero_base_alt'][i_lowb],oxa_meas[i_lowb])[1,0]))
+plt.scatter(oxa_meas7[i_lowb7],gap7['aero_base_alt'][i_lowb7],marker='+',color='tab:orange')
+#        label='AOD>0.1 to AOD<0.3, R={:1.2f}'.format(np.corrcoef(gap7['aero_base_alt'][i_lowb7],oxa_meas7[i_lowb7])[1,0]))
+#pu.plot_lin(oxa_meas7[i_lowa7],gap7['aero_base_alt'][i_lowa7],color='tab:blue',lblfmt='2.4f')
+plt.scatter(oxa_meas8[i_lowb8],gap8['aero_base_alt'][i_lowb8],marker='^',color='tab:orange')
+#            label='AOD>0.1 to AOD<0.3, R={:1.2f}'.format(np.corrcoef(gap8['aero_base_alt'][i_lowb8],oxa_meas8[i_lowb8])[1,0]))
+#pu.plot_lin(oxa_meas8[i_lowa8],gap8['aero_base_alt'][i_lowa8],color='tab:blue',lblfmt='2.4f')
+plt.scatter(oxa_meas[i_lowb],gap6['aero_base_alt'][i_lowb],marker='.',color='tab:orange', 
+            label='AOD>0.1 to AOD<0.3, R={:1.2f}'.format(np.corrcoef(bas_b,oxa_b)[1,0]))
+
+oxa_b = np.append(np.append(oxa_meas[i_lowb],oxa_meas7[i_lowb7]),oxa_meas8[i_lowb8])
+bas_b = np.append(np.append(gap6['aero_base_alt'][i_lowb],gap7['aero_base_alt'][i_lowb7]),gap8['aero_base_alt'][i_lowb8])
+
+pu.plot_lin(oxa_b,bas_b,color='tab:orange',lblfmt='2.4f')
+
+plt.scatter(oxa_meas[i_lowc],gap6['aero_base_alt'][i_lowc],marker='.',color='tab:green')
+#            label='AOD>0.3, R={:1.2f}'.format(np.corrcoef(gap6['aero_base_alt'][i_lowc],oxa_meas[i_lowc])[1,0]))
+plt.scatter(oxa_meas7[i_lowc7],gap7['aero_base_alt'][i_lowc7],marker='+',color='tab:green')
+#        label='AOD>0.3, R={:1.2f}'.format(np.corrcoef(gap7['aero_base_alt'][i_lowc7],oxa_meas7[i_lowc7])[1,0]))
+#pu.plot_lin(oxa_meas7[i_lowa7],gap7['aero_base_alt'][i_lowa7],color='tab:blue',lblfmt='2.4f')
+plt.scatter(oxa_meas8[i_lowc8],gap8['aero_base_alt'][i_lowc8],marker='^',color='tab:green')
+#            label='AOD>0.3, R={:1.2f}'.format(np.corrcoef(gap8['aero_base_alt'][i_lowc8],oxa_meas8[i_lowc8])[1,0]))
+#pu.plot_lin(oxa_meas8[i_lowa8],gap8['aero_base_alt'][i_lowa8],color='tab:blue',lblfmt='2.4f')
+
+oxa_c = np.append(np.append(oxa_meas[i_lowc],oxa_meas7[i_lowc7]),oxa_meas8[i_lowc8])
+bas_c = np.append(np.append(gap6['aero_base_alt'][i_lowc],gap7['aero_base_alt'][i_lowc7]),gap8['aero_base_alt'][i_lowc8])
+plt.scatter(oxa_meas[i_lowc],gap6['aero_base_alt'][i_lowc],marker='.',color='tab:green',
+            label='AOD>0.3, R={:1.2f}'.format(np.corrcoef(bas_c,oxa_c)[1,0]))
+pu.plot_lin(oxa_c,bas_c,color='tab:green',lblfmt='2.4f')
+
+plt.legend()
+plt.xlabel('Depth of Oxygen-A [sum of optical depth deviation]')
+plt.ylabel('Aerosol layer base height [m]')
+plt.xlim(0,200000)
+plt.title('ORACLES [2016-17-18]\nAerosol height, Ox-A, and AOD from 4STAR\n[measured between 850 mb to 930 mb]')
+plt.savefig(fp+'ORACLES_all_OXa_vs_aero_alt_{}.png'.format(vv),dpi=600,transparent=True)
 
 
 # In[ ]:
